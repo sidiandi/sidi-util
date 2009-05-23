0107e8f15ecd620c3dff5569d265c76025265e32
andreas.grimme <andreas.grimme@8a84ef9e-818e-11dd-b2bb-736fb4d8a922>
2009-05-23 16:35:31 +0000

    Automatic build of Sidi.Util

---
+++ tags/1.1.0.99/Sidi.Build/CreateUpdateInfo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Build.Framework;
+using System.Reflection;
+using Sidi.Util;
+
+namespace Sidi.Build
+{
+    public class CreateUpdateInfo : ITask
+    {
+        #region ITask Members
+
+        public IBuildEngine BuildEngine { set; get; }
+
+        public bool Execute()
+        {
+            Assembly a = Assembly.ReflectionOnlyLoadFrom(AssemblyFile);
+            VersionInfo v = new VersionInfo();
+            v.AssemblyName = a.GetName();
+            v.DownloadUrl = DownloadUrl;
+            v.Message = Message;
+            UpdateInfo i = new UpdateInfo();
+            i.VersionInfo.Add(v);
+            i.Write(OutputFile);
+            return true;
+        }
+
+        public string AssemblyFile { set; get; }
+        public string DownloadUrl { set; get; }
+        public string Message { set; get; }
+
+        public string OutputFile { set; get; }
+
+        public ITaskHost HostObject { set; get; }
+
+        #endregion
+    }
+}
