62ebd0a2e2ceb8d2ff4c6203490d8f7c278fbd12
andreas.grimme <andreas.grimme@8a84ef9e-818e-11dd-b2bb-736fb4d8a922>
2009-05-24 03:57:34 +0000

    Automatic build of sidi-util

---
+++ tags/1.1.0.111/Sidi.Build/DebuggingToolsForWindows.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Sidi.IO;
+using System.IO;
+
+namespace Sidi.Build
+{
+    public static class DebuggingToolsForWindows
+    {
+        public static string DownloadUrl
+        {
+            get
+            {
+                return "http://www.microsoft.com/whdc/devtools/debugging/default.mspx";
+            }
+        }
+        
+        public static string Directory
+        {
+            get
+            {
+                string d = FileUtil.CatDir(
+                    Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles),
+                    "Debugging Tools for Windows");
+
+                if (!System.IO.Directory.Exists(d))
+                {
+                    throw new DirectoryNotFoundException(String.Format("The Debugging Tools for Windows must be installed at {0}. See {1}",
+                        d,
+                        DownloadUrl));
+                }
+
+                return d;
+            }
+        }
+    }
+}
