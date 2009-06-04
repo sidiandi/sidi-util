3a606147f10a786d7923420297c1a291c7391508
andreas.grimme <andreas.grimme@8a84ef9e-818e-11dd-b2bb-736fb4d8a922>
2009-06-04 04:24:32 +0000

    Automatic build of sidi-util

---
+++ tags/1.1.0.131/Sidi.Util.Test/LruBackgroundCache.cs
@@ -0,0 +1,99 @@
+// Copyright (c) 2009, Andreas Grimme (http://andreas-grimme.gmxhome.de/)
+// 
+// This file is part of sidi-util.
+// 
+// sidi-util is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// sidi-util is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with sidi-util. If not, see <http://www.gnu.org/licenses/>.
+
+#region "Mandatory NUnit Imports"
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+using NUnit.Framework;
+#endregion
+
+//Test Specific Imports
+//TODO - Add imports your going to test here
+using Sidi.Util;
+using Sidi.Collections;
+
+namespace Sidi.Util.Test
+{
+
+    [TestFixture]
+    public class LruBackgroundCache
+    {
+        #region "Custom Trace Listener"
+        MyListener listener = new MyListener();
+
+        internal class MyListener : TraceListener
+        {
+            public override void Write(string message)
+            {
+                Console.Write(message);
+            }
+
+
+            public override void WriteLine(string message)
+            {
+                Console.WriteLine(message);
+            }
+        }
+        #endregion
+
+
+        [SetUp()]
+        public void SetUp()
+        {
+            //Setup our custom trace listener
+            if (!Trace.Listeners.Contains(listener))
+            {
+                Trace.Listeners.Add(listener);
+            }
+
+            //TODO - Setup your test objects here
+        }
+
+        [TearDown()]
+        public void TearDown()
+        {
+            //Remove our custom trace listener
+            if (Trace.Listeners.Contains(listener))
+            {
+                Trace.Listeners.Remove(listener);
+            }
+
+            //TODO - Tidy up your test objects here
+        }
+
+        [Test()]
+        public void Simple1()
+        {
+            LruCacheBackground<int, int> cache = new LruCacheBackground<int, int>(10, new LruCache<int, int>.ProvideValue(delegate(int key)
+            {
+                Console.WriteLine("Load " + key.ToString());
+                System.Threading.Thread.Sleep(10);
+                return key;
+            }));
+
+            for (int i = 0; i < 1000; ++i)
+            {
+                int value = cache[i];
+            }
+
+            System.Threading.Thread.Sleep(1000);
+        }
+    }
+
+}
