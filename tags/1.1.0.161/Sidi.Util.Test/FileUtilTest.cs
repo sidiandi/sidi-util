e233bf1a7b19b1c0470d23898e66410d2ff067d3
andreas.grimme <andreas.grimme@8a84ef9e-818e-11dd-b2bb-736fb4d8a922>
2009-06-04 04:52:39 +0000

    Automatic build of sidi-util

---
+++ tags/1.1.0.161/Sidi.Util.Test/FileUtilTest.cs
@@ -0,0 +1,115 @@
+// Copyright (c) 2009, Andreas Grimme (http://andreas-grimme.gmxhome.de/)
+// 
+// This file is part of sidi-util.
+// 
+// sidi-util is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// sidi-util is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with sidi-util. If not, see <http://www.gnu.org/licenses/>.
+
+#region "Mandatory NUnit Imports"
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+using NUnit.Framework;
+using Sidi.IO;
+using System.IO;
+#endregion
+
+//Test Specific Imports
+//TODO - Add imports your going to test here
+
+namespace Sidi.Util.Test
+{
+
+    [TestFixture]
+    public class FileUtilTest
+    {
+        #region "Custom Trace Listener"
+        MyListener listener = new MyListener();
+
+        internal class MyListener : TraceListener
+        {
+            public override void Write(string message)
+            {
+                Console.Write(message);
+            }
+
+
+            public override void WriteLine(string message)
+            {
+                Console.WriteLine(message);
+            }
+        }
+        #endregion
+
+
+        [SetUp()]
+        public void SetUp()
+        {
+            //Setup our custom trace listener
+            if (!Trace.Listeners.Contains(listener))
+            {
+                Trace.Listeners.Add(listener);
+            }
+
+            //TODO - Setup your test objects here
+        }
+
+        [TearDown()]
+        public void TearDown()
+        {
+            //Remove our custom trace listener
+            if (Trace.Listeners.Contains(listener))
+            {
+                Trace.Listeners.Remove(listener);
+            }
+
+            //TODO - Tidy up your test objects here
+        }
+
+        [Test()]
+        public void TestGetRelativePath()
+        {
+            Assert.AreEqual(FileUtil.GetRelativePath(@"a\b\c", @"a"), @"b\c");
+            Assert.AreEqual(FileUtil.GetRelativePath(@"a", @"a\b\c"), @"..\..");
+            Assert.AreEqual(FileUtil.GetRelativePath(@".\a", @"."), @"a");
+            Assert.AreEqual(FileUtil.GetRelativePath(@".", @".\a"), @"..");
+
+            Assert.AreEqual(@"a\b\c".GetRelativePath(@"a"), @"b\c");
+            Assert.AreEqual(@"a".GetRelativePath(@"a\b\c"), @"..\..");
+            Assert.AreEqual(@".\a".GetRelativePath(@"."), @"a");
+            Assert.AreEqual(@".".GetRelativePath(@".\a"), @"..");
+        }
+
+        [Test]
+        public void Sibling()
+        {
+            Assert.IsTrue(@"a\b".Sibling("c").EndsWith(@"a\c"));
+        }
+
+        [Test]
+        public void GetFileSystemInfo()
+        {
+            string p = FileUtil.BinFile("test");
+            FileSystemInfo fi = p.GetFileSystemInfo();
+            Assert.AreEqual(fi.FullName, p);
+        }
+
+        [Test]
+        public void CatDir()
+        {
+            Assert.AreEqual(FileUtil.CatDir("a", "b", "c"), @"a\b\c");
+        }
+    }
+
+}
---
+++ tags/1.1.0.161/Sidi.Util/UpdateCheck.cs
@@ -0,0 +1,268 @@
+// Copyright (c) 2009, Andreas Grimme (http://andreas-grimme.gmxhome.de/)
+// 
+// This file is part of sidi-util.
+// 
+// sidi-util is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// sidi-util is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with sidi-util. If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.Xml.Serialization;
+using System.Net;
+using System.IO;
+using Sidi.IO;
+using System.Threading;
+using System.Net.Cache;
+using System.Windows.Forms;
+
+namespace Sidi.Util
+{
+    public class VersionInfo
+    {
+        [XmlIgnore]
+        public AssemblyName AssemblyName
+        {
+            set
+            {
+                Name = value.Name;
+                Version = value.Version.ToString();
+            }
+        }
+
+        public string Name;
+        public string Version;
+        public string DownloadUrl;
+        public string Message;
+    }
+
+    public class UpdateInfo
+    {
+        public List<VersionInfo> VersionInfo = new List<VersionInfo>();
+
+        public void Write(string outFile)
+        {
+            XmlSerializer s = new XmlSerializer(typeof(UpdateInfo));
+            using (Stream f = File.OpenWrite(outFile))
+            {
+                s.Serialize(f, this);
+            }
+        }
+    }
+
+    public class UpdateCheck
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        Uri updateInfoUri;
+        UpdateInfo updateInfo;
+        Assembly assembly;
+
+        public Assembly Assembly
+        {
+            get { return assembly; }
+            set { assembly = value; }
+        }
+
+        public bool IsUpdateRequired
+        {
+            get
+            {
+                return InstalledVersion < AvailableVersion;
+            }
+        }
+
+        public Version InstalledVersion
+        {
+            get
+            {
+                return assembly.GetName().Version;
+            }
+        }
+
+        public Version AvailableVersion
+        {
+            get
+            {
+                return new Version(VersionInfo.Version);
+            }
+        }
+
+        VersionInfo VersionInfo
+        {
+            get
+            {
+                return updateInfo.VersionInfo.First(x => x.Name.Equals(assembly.GetName().Name));
+            }
+        }
+
+        public string DownloadUrl
+        {
+            get { return VersionInfo.DownloadUrl; }
+        }
+
+        public string Message
+        {
+            get { return VersionInfo.Message; }
+        }
+
+        public UpdateInfo UpdateInfo
+        {
+            get { return updateInfo; }
+            set 
+            {
+                updateInfo = value;
+                log.Info("Update info changed.");
+                OnUpdateInfoChanged();
+            }
+        }
+
+        public UpdateCheck(Uri updateInfoUri)
+            : this(Assembly.GetCallingAssembly(), updateInfoUri)
+        {
+        }
+
+        
+        public UpdateCheck(Assembly assembly, Uri updateInfoUri)
+        {
+            if (assembly == null)
+            {
+                throw new ArgumentNullException("assembly");
+            }
+            if (updateInfoUri == null)
+            {
+                throw new ArgumentNullException("updateInfoUri");
+            }
+            this.updateInfoUri = updateInfoUri;
+            this.assembly = assembly;
+        }
+
+        public void Check()
+        {
+            try
+            {
+                ReadUpdateInfo(false);
+            }
+            catch (Exception e)
+            {
+                log.Error("Error during update check", e);
+            }
+        }
+
+        Action updateRequiredHandler;
+
+        public void CheckAsync()
+        {
+            CheckAsync(delegate()
+            {
+                FormCollection of = Application.OpenForms;
+                if (of.Count > 0)
+                {
+                    of[0].Invoke(new Action(delegate()
+                    {
+                        UpdateForm u = new UpdateForm(this);
+                        u.ShowDialog();
+                    }));
+                }
+                else
+                {
+                    UpdateForm u = new UpdateForm(this);
+                    u.ShowDialog();
+                }
+            });
+        }
+        
+        public void CheckAsync(Action updateRequiredHandler)
+        {
+            this.updateRequiredHandler = updateRequiredHandler;
+            completed = false;
+            ReadUpdateInfo(true);
+        }
+
+        bool completed = true;
+
+        public void WaitCompleted()
+        {
+            while (!completed)
+            {
+                Thread.Sleep(100);
+            }
+        }
+
+        Uri uri;
+
+        public void ReadUpdateInfo(bool async)
+        {
+            WebClient web = new WebClient();
+            web.CachePolicy = new RequestCachePolicy(RequestCacheLevel.NoCacheNoStore);
+            UriBuilder uriBuilder = new UriBuilder(updateInfoUri);
+            if (!updateInfoUri.IsFile)
+            {
+                AssemblyName an = assembly.GetName();
+                uriBuilder.Query = "name=" + an.Name + "&" + "version=" + an.Version.ToString();
+            }
+            uri = uriBuilder.Uri;
+            log.InfoFormat("Downloading update info from {0}", uri);
+            if (async)
+            {
+                web.DownloadDataCompleted += new DownloadDataCompletedEventHandler(web_DownloadDataCompleted);
+                web.DownloadDataAsync(uri);
+            }
+            else
+            {
+                Parse(web.DownloadData(uri));
+            }
+        }
+
+        void web_DownloadDataCompleted(object sender, DownloadDataCompletedEventArgs e)
+        {
+            if (e.Error != null)
+            {
+                log.Error(String.Format("Error while downloading {0}", uri), e.Error);
+            }
+            else
+            {
+                try
+                {
+                    Parse(e.Result);
+                }
+                catch (Exception ex)
+                {
+                    log.Error("Error while processing update info", ex);
+                }
+            }
+            completed = true;
+        }
+
+        void Parse(byte[] data)
+        {
+            MemoryStream m = new MemoryStream(data);
+            XmlSerializer s = new XmlSerializer(typeof(UpdateInfo));
+            UpdateInfo = (UpdateInfo)s.Deserialize(m);
+        }
+
+        protected virtual void OnUpdateInfoChanged()
+        {
+            XmlSerializer s = new XmlSerializer(typeof(UpdateInfo));
+            s.Serialize(Console.Out, UpdateInfo);
+
+            if (IsUpdateRequired)
+            {
+                updateRequiredHandler();
+            }
+        }
+    }
+
+}
