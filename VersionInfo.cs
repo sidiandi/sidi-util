943523734b90fcbacb8c7fb0e6aca99aa2623c7f
andreas.grimme <sidiandi@users.noreply.github.com>
2009-06-09 08:00:08 +0000

    war nur eine klammer verrutscht   in der Methode WriteUsage in Zeile 682

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -14,7 +14,7 @@ using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.1.0.163")]
-[assembly: AssemblyFileVersion("1.1.0.163")]
+[assembly: AssemblyVersion("1.1.0.166")]
+[assembly: AssemblyFileVersion("1.1.0.166")]
 
 
