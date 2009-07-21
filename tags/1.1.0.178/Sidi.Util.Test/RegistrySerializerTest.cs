ab2326f61cccb69464be1b3483505f9269fbfc70
andreas.grimme <andreas.grimme@8a84ef9e-818e-11dd-b2bb-736fb4d8a922>
2009-07-21 10:51:02 +0000

    Automatic build of sidi-util

---
+++ tags/1.1.0.178/Sidi.Util.Test/RegistrySerializerTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Sidi.Util.Test
+{
+    [TestFixture]
+    public class RegistrySerializerTest
+    {
+        public class Example
+        {
+            public string AStringVariable;
+            public bool ABoolVariable;
+            public int AIntVariable;
+        }
+
+        [Test]
+        public void ReadWrite()
+        {
+            var x = new Example();
+            x.AStringVariable = "Hello";
+            x.ABoolVariable = true;
+            x.AIntVariable = 123;
+
+            string key = @"HKEY_CURRENT_USER\Software\sidi-util\Test\Example";
+            RegistrySerializer.Write(key, x);
+
+            var y = new Example();
+            RegistrySerializer.Read(key, y);
+            Assert.AreEqual(x.AStringVariable, y.AStringVariable);
+            Assert.AreEqual(x.AIntVariable, y.AIntVariable);
+            Assert.AreEqual(x.ABoolVariable, y.ABoolVariable);
+        }
+    }
+}
