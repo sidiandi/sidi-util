9e6232f0aec3293fc45690137d7ec0b890966e0d
andreas.grimme <andreas.grimme@8a84ef9e-818e-11dd-b2bb-736fb4d8a922>
2009-07-26 15:53:48 +0000

    Automatic build of sidi-util

---
+++ tags/1.1.0.183/Sidi.Build/SourceIndexTest.cs
@@ -0,0 +1,62 @@
+// Copyright (c) 2009, Andreas Grimme (http://andreas-grimme.gmxhome.de/)
+// 
+// This file is part of sidi-util.
+// 
+// sidi-util is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// sidi-util is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with sidi-util. If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using Sidi.IO;
+
+namespace Sidi.Build.Test
+{
+    [TestFixture]
+    public class SourceIndexTest
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        SourceIndex instance = null;
+
+        public SourceIndexTest()
+        {
+            log4net.Config.BasicConfigurator.Configure();
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            instance = new SourceIndex();
+        }
+
+        [Test]
+        public void Test()
+        {
+            instance.Directory = @"D:\work\Sidi.Util";
+            instance.Url = "http://sidi-util.googlecode.com/svn/trunk";
+
+            TaskItem t = new TaskItem(@"D:\temp\Sidi.Util.dll");
+            instance.Modules = new ITaskItem[] { t };
+
+            instance.Execute();
+
+            Srctool s = new Srctool();
+            s.Extract(t.ItemSpec.ReplaceExtension("pdb"));
+        }
+    }
+}
---
+++ tags/1.1.0.183/Sidi.Util.Test/DumpExtensionsTest.cs
@@ -0,0 +1,51 @@
+// Copyright (c) 2009, Andreas Grimme (http://andreas-grimme.gmxhome.de/)
+// 
+// This file is part of sidi-util.
+// 
+// sidi-util is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// sidi-util is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with sidi-util. If not, see <http://www.gnu.org/licenses/>.
+
+#region "Mandatory NUnit Imports"
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+using NUnit.Framework;
+#endregion
+
+//Test Specific Imports
+//TODO - Add imports your going to test here
+
+namespace Sidi.Util.Test
+{
+    public class Test
+    {
+        public Test()
+        {
+            log4net.Config.BasicConfigurator.Configure();
+        }
+    }
+    
+    [TestFixture]
+    public class DumpExtensionsTest : Test
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        [Test]
+        public void DumpProperties()
+        {
+            Process p = Process.GetCurrentProcess();
+            p.DumpProperties(Console.Out);
+        }
+    }
+}
