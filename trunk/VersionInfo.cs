7bd147685f2c6d6cb93af012366638d90d728732
andreas.grimme <andreas.grimme@8a84ef9e-818e-11dd-b2bb-736fb4d8a922>
2009-09-13 17:36:44 +0000

    Auto commit from MSBuild.

--- trunk/VersionInfo.cs
+++ trunk/VersionInfo.cs
@@ -14,7 +14,7 @@ using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.1.0.220")]
-[assembly: AssemblyFileVersion("1.1.0.220")]
+[assembly: AssemblyVersion("1.1.0.222")]
+[assembly: AssemblyFileVersion("1.1.0.222")]
 
 
