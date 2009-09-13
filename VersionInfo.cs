019b1172e03b0a9de52b0aba407cbc3c9b860111
andreas.grimme <sidiandi@users.noreply.github.com>
2009-09-13 17:33:55 +0000

    Auto commit from MSBuild.

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -14,7 +14,7 @@ using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.1.0.218")]
-[assembly: AssemblyFileVersion("1.1.0.218")]
+[assembly: AssemblyVersion("1.1.0.220")]
+[assembly: AssemblyFileVersion("1.1.0.220")]
 
 
