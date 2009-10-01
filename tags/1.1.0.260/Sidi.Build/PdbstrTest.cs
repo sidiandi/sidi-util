d12097cc3526ce0ee05fe81c04d3678e4cfa75ca
andreas.grimme <andreas.grimme@8a84ef9e-818e-11dd-b2bb-736fb4d8a922>
2009-10-01 04:47:14 +0000

    Automatic build of sidi-util

---
+++ tags/1.1.0.260/Sidi.Build/PdbstrTest.cs
@@ -0,0 +1,63 @@
+// Copyright (c) 2009, Andreas Grimme (http://andreas-grimme.gmxhome.de/)
+// 
+// This file is part of sidi-util.
+// 
+// sidi-util is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// sidi-util is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with sidi-util. If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Sidi.IO;
+using System.IO;
+using Sidi.Util;
+
+namespace Sidi.Build.Test
+{
+    [TestFixture]
+    public class PdbstrTest
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        Pdbstr instance = null;
+
+        public PdbstrTest()
+        {
+            log4net.Config.BasicConfigurator.Configure();
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            instance = new Pdbstr();
+        }
+
+        string pdbFile = FileUtil.BinFile(@"Test\Sidi.Util.pdb");
+
+        [Test]
+        public void Write()
+        {
+            string pdbFileCopy = pdbFile + ".modified.pdb";
+            File.Copy(pdbFile, pdbFileCopy, true);
+
+            string content = "Hello, Test";
+            string streamName = "srcsrv";
+
+            instance.Write(pdbFileCopy, streamName, content);
+
+            Assert.IsTrue(instance.Read(pdbFileCopy, streamName).StartsWith(content));
+        }
+    }
+}
