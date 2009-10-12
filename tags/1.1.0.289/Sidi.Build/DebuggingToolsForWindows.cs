2c5a7ce99320ddc927ebf4d23727da8c346fe2a0
andreas.grimme <andreas.grimme@8a84ef9e-818e-11dd-b2bb-736fb4d8a922>
2009-10-12 07:35:07 +0000

    Automatic build of sidi-util

---
+++ tags/1.1.0.289/Sidi.Build/DebuggingToolsForWindows.cs
@@ -0,0 +1,60 @@
+// Copyright (c) 2009, Andreas Grimme (http://andreas-grimme.gmxhome.de/)
+// 
+// This file is part of sidi-util.
+// 
+// sidi-util is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// sidi-util is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with sidi-util. If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Sidi.IO;
+using System.IO;
+
+namespace Sidi.Build
+{
+    public static class DebuggingToolsForWindows
+    {
+        public static string DownloadUrl
+        {
+            get
+            {
+                return "http://www.microsoft.com/whdc/devtools/debugging/default.mspx";
+            }
+        }
+        
+        public static string Directory
+        {
+            get
+            {
+                string[] searchPath = new string[]
+                {
+                    FileUtil.CatDir(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles), "Debugging Tools for Windows"),
+                    FileUtil.CatDir(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles), "Debugging Tools for Windows (x86)"),
+                };
+
+                foreach (string d in searchPath)
+                {
+                    if (System.IO.Directory.Exists(d))
+                    {
+                        return d;
+                    }
+                }
+
+                throw new DirectoryNotFoundException(String.Format("The Debugging Tools for Windows must be installed at {0}. See {1}",
+                    searchPath[0], DownloadUrl));
+            }
+        }
+    }
+}
