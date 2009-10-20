325b82c8f63107b7549d0346e146d734df4fc047
andreas.grimme <andreas.grimme@8a84ef9e-818e-11dd-b2bb-736fb4d8a922>
2009-10-20 17:35:55 +0000

    Auto commit from MSBuild.

--- trunk/Sidi.Util/ListEx.cs
+++ trunk/Sidi.Util/ListEx.cs
@@ -1,47 +1,24 @@
-// Copyright (c) 2009, Andreas Grimme (http://andreas-grimme.gmxhome.de/)
-// 
-// This file is part of sidi-util.
-// 
-// sidi-util is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-// 
-// sidi-util is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU Lesser General Public License for more details.
-// 
-// You should have received a copy of the GNU Lesser General Public License
-// along with sidi-util. If not, see <http://www.gnu.org/licenses/>.
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.IO;
-using System.Text.RegularExpressions;
-using System.Diagnostics;
-using Sidi.IO;
 
 namespace Sidi.Util
 {
     public static class ListEx
     {
-        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
-
         public static bool Pop<T>(this IList<T> list, out T result)
         {
-            if (list.Count > 0)
+            if (list.Count == 0)
+            {
+                result = default(T);
+                return false;
+            }
+            else
             {
                 result = list[list.Count - 1];
                 list.RemoveAt(list.Count - 1);
                 return true;
-            }
-            else
-            {
-                result = default(T);
-                return false;
             }
         }
     }
