96bf4cdc923552fd8eb269370f8033f8231f4318
andreas.grimme <andreas.grimme@8a84ef9e-818e-11dd-b2bb-736fb4d8a922>
2009-12-30 10:43:20 +0000

    Automatic build of sidi-util

---
+++ tags/1.1.0.326/Sidi.Util.Test/CommandLine/ParserTest.cs
@@ -0,0 +1,363 @@
+// Copyright (c) 2009, Andreas Grimme (http://andreas-grimme.gmxhome.de/)
+// 
+// This file is part of sidi-util.
+// 
+// sidi-util is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// sidi-util is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with sidi-util. If not, see <http://www.gnu.org/licenses/>.
+
+#region "Mandatory NUnit Imports"
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+using NUnit.Framework;
+#endregion
+
+//Test Specific Imports
+//TODO - Add imports your going to test here
+using Sidi.CommandLine;
+using cm = System.ComponentModel;
+using System.IO;
+using Sidi.Util;
+using System.Linq;
+
+namespace Sidi.CommandLine
+{
+    [TestFixture]
+    public class ParserTest : TestBase
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        [SetUp()]
+        public void SetUp()
+        {
+        }
+
+        [TearDown()]
+        public void TearDown()
+        {
+        }
+
+        public enum Fruit
+        {
+            Apple,
+            Orange,
+            Pear,
+        };
+
+        [Usage("App usage")]
+        class TestApp
+        {
+            [Usage("sample action")]
+            public void SayHello()
+            {
+                Result = "Hello, " + Name;
+                Console.WriteLine(Result);
+            }
+
+            [Usage("Determines to whom to say hello.")]
+            public string Name
+            {
+                get { return m_name; }
+                set { m_name = value; }
+            }
+            public string m_name = "John Doe";
+
+            [Usage("Determines how often to say hello.")]
+            public int Times
+            {
+                get { return m_Times; }
+                set { m_Times = value; }
+            }
+            public int m_Times = 1;
+
+
+            [Usage("Determines when to say hello.")]
+            public DateTime Time
+            {
+                get { return m_Time; }
+                set { m_Time = value; }
+            }
+
+            public DateTime m_Time;
+
+            [Usage("Demonstrates enums")]
+            [cm.Category("Advanced")]
+            public Fruit Fruit { set; get; }
+
+            public string Result;
+        }
+
+        [Usage("Minimal app")]
+        public class MinimalTestApp
+        {
+        }
+
+        [Test]
+        public void Enum()
+        {
+            TestApp t = new TestApp();
+            Parser.Run(t, new string[] { "Fruit", "Apple" });
+            Assert.AreEqual(t.Fruit, Fruit.Apple);
+
+            Parser.Run(t, new string[] { "Fruit", "Orange" });
+            Assert.AreEqual(t.Fruit, Fruit.Orange);
+
+            Parser.Run(t, new string[] { "--Fruit", "Pear" });
+            Assert.AreEqual(t.Fruit, Fruit.Pear);
+
+            var usage = StringEx.ToString(new Parser(t).WriteUsage);
+            Assert.IsTrue(usage.Contains(Fruit.Apple.ToString()));
+        }
+
+        [Test]
+        public void ShowUsage()
+        {
+            Parser p = new Parser(new TestApp());
+            var usage = StringEx.ToString(x => p.WriteUsage(x));
+            log.Info(usage);
+            Assert.IsTrue(usage.Contains("Options"));
+            Assert.IsTrue(usage.Contains("Actions"));
+        }
+
+        [Test]
+        public void Info()
+        {
+            TestApp app = new TestApp();
+            Parser p = new Parser(app);
+            string i = p.Info;
+            Console.WriteLine(i);
+            Assert.IsTrue(i.Contains(app.GetType().Name));
+        }
+
+        [Test]
+        public void Help()
+        {
+            TestApp app = new TestApp();
+            Parser.Run(app, new string[] { "help", "SayHello" });
+        }
+
+        [Test()]
+        public void TestAssertion()
+        {
+            TestApp app = new TestApp();
+
+            Sidi.CommandLine.Parser.Run(app, new string[] { "SayHello" });
+            Assert.AreEqual("Hello, John Doe", app.Result);
+
+            Sidi.CommandLine.Parser.Run(app, new string[] { "--Name", "Andreas", "SayHello" });
+            Assert.AreEqual("Hello, Andreas", app.Result);
+
+            Sidi.CommandLine.Parser.Run(app, new string[] { "--Times", "10", "--Name", "Andreas", "SayHello" });
+            Assert.AreEqual("Hello, Andreas", app.Result);
+
+            Sidi.CommandLine.Parser.Run(app, new string[] { "--Name", "Andreas", "SayHello", "--Name", "Ernie", "SayHello" });
+            Assert.AreEqual("Hello, Ernie", app.Result);
+        }
+
+        [Test()]
+        public void TestGui()
+        {
+            TestApp app = new TestApp();
+            Sidi.CommandLine.Parser.Run(app, new string[] { });
+        }
+
+        [Test]
+        public void FuzzySearch()
+        {
+            TestApp app = new TestApp();
+            Sidi.CommandLine.Parser.Run(app, new string[] { "--n", "Andreas", "SayHello" });
+            Assert.AreEqual("Hello, Andreas", app.Result);
+
+            app = new TestApp();
+            Sidi.CommandLine.Parser.Run(app, new string[] { "--n", "Andreas", "sh" });
+            Assert.AreEqual("Hello, Andreas", app.Result);
+
+            app = new TestApp(); 
+            Sidi.CommandLine.Parser.Run(app, new string[] { "--n", "Andreas", "say" });
+            Assert.AreEqual("Hello, Andreas", app.Result);
+
+            app = new TestApp(); 
+            Sidi.CommandLine.Parser.Run(app, new string[] { "-na", "Andreas", "say" });
+            Assert.AreEqual("Hello, Andreas", app.Result);
+        }
+
+        [Test]
+        public void DateTimeArg()
+        {
+            TestApp app = new TestApp();
+
+            Sidi.CommandLine.Parser.Run(app, new string[] { "--time", "2008-05-11 11:12:00" });
+            Assert.IsTrue(app.Time == new DateTime(2008, 5, 11, 11, 12, 0));
+        }
+
+        [Test, ExpectedException(ExceptionType = typeof(CommandLineException))]
+        public void NotUnique()
+        {
+            TestApp app = new TestApp();
+            Parser parser = new Parser(app);
+            // --t is not unique, since it could mean --Time or --Times
+            parser.Parse(new string[] { "--t", "2008-05-11 11:12:00" });
+        }
+
+        [System.ComponentModel.Description("Test app that uses the Description attribute")]
+        class TestAppWithDescription
+        {
+            [System.ComponentModel.Description("some action")]
+            public void SomeAction()
+            {
+            }
+
+            [System.ComponentModel.Description("some option - marker_for_test_aoifq7ft48q")]
+            public string SomeOption { get; set; }
+        }
+
+        [Test]
+        public void TestDescription()
+        {
+            Parser parser = new Parser(new TestAppWithDescription());
+            parser.Parse(new string[] { "--SomeOption", "hello", "SomeAction" });
+
+            StringWriter w = new StringWriter();
+            parser.PrintSampleScript(w);
+            Assert.IsTrue(w.ToString().Contains("marker_for_test_aoifq7ft48q"));
+        }
+
+        [Usage("")]
+        public class TestAppWithStringList
+        {
+            [Usage("process all following arguments")]
+            public void Action(List<string> args)
+            {
+                args.Clear();
+            }
+
+            public void Add(int x, int y)
+            {
+            }
+
+            [Usage("adds a list of numbers")]
+            public void AddList(List<string> list)
+            {
+                log.Info(list.Select(x => Double.Parse(x)).Aggregate(0.0, (x, y) => x + y));
+                list.Clear();
+            }
+
+            [Usage("adds")]
+            public void Add(double x, double y)
+            {
+            }
+
+            [Usage("adds")]
+            public void SubtractInt(int x, int y)
+            {
+            }
+
+            [Usage("adds")]
+            public void SubtractDouble(int x, int y)
+            {
+            }
+        }
+
+        [Test]
+        public void List()
+        {
+            var a = new TestAppWithStringList();
+            var p = new Parser(a);
+            p.Parse(new string[]{ "AddList", "1", "2", "3"});
+        }
+
+        [Test]
+        public void List2()
+        {
+            var a = new TestAppWithStringList();
+            var p = new Parser(a);
+            p.Parse(new string[] { "AddList" });
+        }
+
+        [Test]
+        public void WrongParameterCountMessage()
+        {
+            var p = new Parser(new TestAppWithStringList());
+            try
+            {
+                p.Parse(new string[] { "Add", "1" });
+            }
+            catch (CommandLineException e)
+            {
+                log.Info(e.Message);
+                Assert.IsTrue(e.Message.Contains(p.GetAction("Add").Usage));
+            }
+        }
+
+        [Test]
+        public void ArgList()
+        {
+            Parser parser = new Parser(new TestAppWithStringList());
+            parser.Parse(new string[] { "Action", "arg1", "arg2", "arg3" });
+        }
+
+        [Test]
+        public void Ambiguous()
+        {
+            Parser parser = new Parser(new TestAppWithStringList());
+            parser.Parse(new string[] { "Add", "1.0", "1.0" });
+        }
+
+        [Test]
+        public void Ambiguous2()
+        {
+            Parser.Run(new TestAppWithStringList(), new string[] { "Subtract", "1", "1" });
+        }
+
+        [Test]
+        public void NotEnoughParameters()
+        {
+            Parser.Run(new TestAppWithStringList(), new string[] { "Add", "1" });
+        }
+
+        [Usage("Multiline test")]
+        public class TestMultiLineUsage
+        {
+            [Usage("multiline\r\nline 1\r\nline2\r\nline3")]
+            public void A()
+            {
+            }
+
+            [Usage("very long line that should be wrapped. very long line that should be wrapped. very long line that should be wrapped. very long line that should be wrapped. very long line that should be wrapped. very long line that should be wrapped. very long line that should be wrapped. very long line that should be wrapped. very long line that should be wrapped. very long line that should be wrapped. very long line that should be wrapped. very long line that should be wrapped. ")]
+            public void B()
+            {
+            }
+        }
+
+        [Test]
+        public void ShowMultiLineUsage()
+        {
+            string u = StringEx.ToString(new Parser(new TestMultiLineUsage()).WriteUsage);
+            Console.WriteLine(u);
+        }
+
+        [Test]
+        public void IsMatch()
+        {
+            Parser p = new Parser(new TestMultiLineUsage());
+            Assert.IsTrue(p.IsMatch("rfl", "RunFromLocal"));
+            Assert.IsFalse(p.IsMatch("rfl", "RunFromlocal"));
+            Assert.IsTrue(p.IsMatch("runfr", "RunFromLocal"));
+            Assert.IsTrue(p.IsMatch("runfromlocal", "RunFromLocal"));
+            Assert.IsTrue(p.IsMatch("rufro", "RunFromLocal"));
+            Assert.IsTrue(p.IsMatch("rfrlocal", "RunFromLocal"));
+            Assert.IsFalse(p.IsMatch("rfrlocale", "RunFromLocal"));
+        }
+    }
+}
