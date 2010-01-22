0156334873c794b03bfe9912b4655f5dbc1ea23d
andreas.grimme <andreas.grimme@8a84ef9e-818e-11dd-b2bb-736fb4d8a922>
2010-01-22 16:49:02 +0000

    Automatic build of sidi-util

---
+++ tags/1.1.0.354/Sidi.Util/ListEx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sidi.Util
+{
+    public static class ListEx
+    {
+        public static bool Pop<T>(this IList<T> list, out T result)
+        {
+            if (list.Count == 0)
+            {
+                result = default(T);
+                return false;
+            }
+            else
+            {
+                result = list[list.Count - 1];
+                list.RemoveAt(list.Count - 1);
+                return true;
+            }
+        }
+    }
+}
---
+++ tags/1.1.0.354/Sidi.Util/RandomEx.cs
@@ -0,0 +1,52 @@
+// Copyright (c) 2009, Andreas Grimme (http://andreas-grimme.gmxhome.de/)
+// 
+// This file is part of sidi-util.
+// 
+// sidi-util is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// sidi-util is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with sidi-util. If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Sidi.Util
+{
+    public static class RandomEx
+    {
+        public static string String(this Random random, int length)
+        {
+            return String(random, length, @"[0-9a-zA-Z]");
+        }
+
+        public static string String(this Random random, int length, string allowedCharacterRegex)
+        {
+            List<String> allowed = new List<String>();
+            Regex r = new Regex(allowedCharacterRegex);
+            for (char c = Char.MinValue; c < (char)256; ++c)
+            {
+                String s = new String(new char[] { c });
+                if (r.Match(s).Success)
+                {
+                    allowed.Add(s);
+                }
+            }
+            StringBuilder b = new StringBuilder();
+            for (int i = 0; i < length; ++i)
+            {
+                b.Append(allowed[random.Next(0, allowed.Count)]);
+            }
+            return b.ToString();
+        }
+    }
+}
