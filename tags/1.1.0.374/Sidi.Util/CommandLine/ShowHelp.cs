96b742cddf58487ef469d11e0aa053b97614b71d
andreas.grimme <andreas.grimme@8a84ef9e-818e-11dd-b2bb-736fb4d8a922>
2010-01-24 12:55:44 +0000

    Automatic build of sidi-util

---
+++ tags/1.1.0.374/Sidi.Util/CommandLine/ShowHelp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Sidi.CommandLine
+{
+    public class ShowHelp
+    {
+        Parser parser;
+
+        public ShowHelp(Parser parser)
+        {
+            this.parser = parser;
+        }
+
+        [Usage("Shows help for all options and actions that match searchString")]
+        public void Help(string searchString)
+        {
+            foreach (var i in parser.Items)
+            {
+                if (Regex.IsMatch(i.UsageText, searchString, RegexOptions.IgnoreCase))
+                {
+                    Console.WriteLine();
+                    Console.WriteLine(i.UsageText);
+                }
+            }
+        }
+    }
+
+}
