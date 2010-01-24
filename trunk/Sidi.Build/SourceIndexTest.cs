d8ea12e25c11cd90b66728d80c3990aa2ec064eb
andreas.grimme <andreas.grimme@8a84ef9e-818e-11dd-b2bb-736fb4d8a922>
2010-01-24 19:36:58 +0000

    tests fixed

--- trunk/Sidi.Build/SourceIndexTest.cs
+++ trunk/Sidi.Build/SourceIndexTest.cs
@@ -27,7 +27,7 @@ using Sidi.IO;
 namespace Sidi.Build.Test
 {
     [TestFixture]
-    public class SourceIndexTest
+    public class SourceIndexTest : TestBase
     {
         private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
@@ -44,13 +44,13 @@ namespace Sidi.Build.Test
             instance = new SourceIndex();
         }
 
-        [Test]
+        [Test, Explicit("requires source path")]
         public void Test()
         {
-            instance.Directory = @"D:\work\Sidi.Util";
+            instance.Directory = @"E:\work\sidi-util";
             instance.Url = "http://sidi-util.googlecode.com/svn/trunk";
 
-            TaskItem t = new TaskItem(@"D:\temp\Sidi.Util.dll");
+            TaskItem t = new TaskItem(TestFile("Sidi.Util.dll"));
             instance.Modules = new ITaskItem[] { t };
 
             instance.Execute();
