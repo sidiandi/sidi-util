b37b2cc2cd4f659beba24818945929eaea2cdf65
andreas.grimme@googlemail.com <andreas.grimme@googlemail.com@8a84ef9e-818e-11dd-b2bb-736fb4d8a922>
2011-05-21 01:36:22 +0000

    command line parser SubCommands and return values

---
+++ trunk/Sidi.Util.Test/CommandLine/SubCommand.cs
@@ -0,0 +1,69 @@
+﻿// Copyright (c) 2009, Andreas Grimme (http://andreas-grimme.gmxhome.de/)
+// 
+// This file is part of sidi-util.
+// 
+// sidi-util is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// sidi-util is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with sidi-util. If not, see <http://www.gnu.org/licenses/>.
+
+#region "Mandatory NUnit Imports"
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+using NUnit.Framework;
+#endregion
+
+//Test Specific Imports
+//TODO - Add imports your going to test here
+using Sidi.CommandLine;
+using cm = System.ComponentModel;
+using System.IO;
+using Sidi.Util;
+using System.Linq;
+
+namespace Sidi.CommandLine
+{
+    [TestFixture]
+    public class SubCommandTest : TestBase
+    {
+        [Usage("Test app")]
+        public class App
+        {
+            [SubCommand]
+            public Math Math;
+        }
+
+        [Usage("Mathematical operations")]
+        public class Math
+        {
+            [Usage("adds a and b")]
+            public int Add(int a, int b)
+            {
+                return a + b;
+            }
+            [Usage("multiply a and b")]
+            public int Multiply(int a, int b)
+            {
+                return a * b;
+            }
+        }
+
+        [Test]
+        public void Sub()
+        {
+            var app = new App();
+            var p = new Parser(app);
+            p.Parse(new string[] { "Math", "Add", "1", "1", "Multiply", "10", "10"});
+        }
+    }
+}
