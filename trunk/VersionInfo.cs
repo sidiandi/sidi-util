b15db40bccf8737dd5138b0f26881ba73e24d665
andreas.grimme@googlemail.com <andreas.grimme@googlemail.com@8a84ef9e-818e-11dd-b2bb-736fb4d8a922>
2012-11-04 08:02:05 +0000

    2.4.0

--- trunk/VersionInfo.cs
+++ trunk/VersionInfo.cs
@@ -16,7 +16,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright (c) Andreas Grimme 2010")]
 [assembly: AssemblyCompany("sidi")]
 [assembly: AssemblyProduct("sidi-util")]
-[assembly: AssemblyVersion("2.3.0.544")]
-[assembly: AssemblyFileVersion("2.3.0.544")]
+[assembly: AssemblyVersion("2.3.0.546")]
+[assembly: AssemblyFileVersion("2.3.0.546")]
 
 
