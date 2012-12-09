c77bcd3fa892d218cfd57e9ea68428850799fa4f
andreas.grimme@googlemail.com <andreas.grimme@googlemail.com@8a84ef9e-818e-11dd-b2bb-736fb4d8a922>
2012-12-09 11:11:07 +0000

    fixed LPath IXmlSerializable implementation

--- trunk/VersionInfo.cs
+++ trunk/VersionInfo.cs
@@ -16,7 +16,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright (c) Andreas Grimme 2010")]
 [assembly: AssemblyCompany("sidi")]
 [assembly: AssemblyProduct("sidi-util")]
-[assembly: AssemblyVersion("2.4.0.551")]
-[assembly: AssemblyFileVersion("2.4.0.551")]
+[assembly: AssemblyVersion("2.4.0.555")]
+[assembly: AssemblyFileVersion("2.4.0.555")]
 
 
