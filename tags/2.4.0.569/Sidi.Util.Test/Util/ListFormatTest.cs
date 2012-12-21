420c6da930226630df7ec2ff495234f3691491cb
andreas.grimme@googlemail.com <andreas.grimme@googlemail.com@8a84ef9e-818e-11dd-b2bb-736fb4d8a922>
2012-12-21 13:58:55 +0000

    Automatic build of sidi-util

---
+++ tags/2.4.0.569/Sidi.Util.Test/Util/ListFormatTest.cs
@@ -0,0 +1,58 @@
+// Copyright (c) 2009, Andreas Grimme (http://andreas-grimme.gmxhome.de/)
+// 
+// This file is part of sidi-util.
+// 
+// sidi-util is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// sidi-util is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with sidi-util. If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using System.IO;
+using Sidi.Extensions;
+
+namespace Sidi.Util
+{
+    [TestFixture]
+    public class ListFormatTest : TestBase
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        [Test]
+        public void Test()
+        {
+            var data = new DirectoryInfo(TestFile(".")).GetFiles();
+
+            {
+                data.ListFormat()
+                    .AddColumn("Name", f => f.Name)
+                    .AddColumn("Date", f => f.LastWriteTime)
+                    .RenderText(Console.Out);
+            }
+
+            {
+                data.ListFormat()
+                    .Property("Name", "LastWriteTime")
+                    .RenderText(Console.Out);
+            }
+
+            {
+                data.ListFormat()
+                    .PropertyColumns()
+                    .RenderText(Console.Out);
+            }
+        }
+    }
+}
