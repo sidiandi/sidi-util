54aa3b054d4064d17dfba9a97b8146d95f5b91a1
andreas.grimme@googlemail.com <andreas.grimme@googlemail.com@8a84ef9e-818e-11dd-b2bb-736fb4d8a922>
2012-12-22 21:54:57 +0000

    Automatic build of sidi-util

---
+++ tags/2.4.0.600/Sidi.Util/CommandLine/ActionDialog.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Sidi.CommandLine
+{
+    public class ActionDialog : Form
+    {
+        public void Execute()
+        {
+            ActionTag.Execute();
+        }
+
+        public void ShowAndExecute()
+        {
+            for (; ; )
+            {
+                if (ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    Execute();
+                    break;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+        public ActionTag ActionTag;
+    }
+
+    public class ActionTag
+    {
+        public ActionTag(Action action)
+        {
+            Action = action;
+        }
+        public List<TextBox> ParameterTextBoxes = new List<TextBox>();
+        public Action Action { get; private set; }
+        public Button Button { get; set; }
+
+        [SuppressMessage("Microsoft.Design", "CA1031")]
+        public void Execute()
+        {
+            try
+            {
+                var p = ParameterTextBoxes.Select(x => x.Text).ToList();
+                Action.Handle(p, true);
+                ShowUserInterface.ClearError(Button);
+            }
+            catch (Exception ex)
+            {
+                var msg = ex.InnerException == null ? ex.Message : ex.InnerException.Message;
+                ShowUserInterface.SetError(Button, msg);
+                throw;
+            }
+        }
+    }
+
+}
