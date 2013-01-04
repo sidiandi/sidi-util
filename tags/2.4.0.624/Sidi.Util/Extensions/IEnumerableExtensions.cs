1ccdec396df0483f0109ccbea93fa11ef736b6e4
andreas.grimme@googlemail.com <andreas.grimme@googlemail.com@8a84ef9e-818e-11dd-b2bb-736fb4d8a922>
2013-01-04 22:03:44 +0000

    Automatic build of sidi-util

---
+++ tags/2.4.0.624/Sidi.Util/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,139 @@
+// Copyright (c) 2012, Andreas Grimme (http://andreas-grimme.gmxhome.de/)
+// 
+// This file is part of sidi-util.
+// 
+// sidi-util is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// sidi-util is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with sidi-util. If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Sidi.Extensions
+{
+    public static class IEnumerableExtensions
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        /// <summary>
+        /// Returns a counted (0-based) enumeration.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="e"></param>
+        /// <returns></returns>
+        [SuppressMessage("Microsoft.Design", "CA1006:DoNotNestGenericTypesInMemberSignatures")]
+        public static IEnumerable<KeyValuePair<int, T>> Counted<T>(this IEnumerable<T> e)
+        {
+            int index = 0;
+            return e.Select(i => new KeyValuePair<int, T>(index++, i));
+        }
+
+        public static IEnumerable<T> Chain<T>(T i, Func<T, T> transform)
+        {
+            for (; i != null; i = transform(i))
+            {
+                yield return i;
+            }
+        }
+
+        public static IEnumerable<Y> AggregateSelect<Y, X>(this IEnumerable<X> sequence, Y initialResult, Func<Y, X, Y> selector)
+        {
+            foreach (var i in sequence)
+            {
+                initialResult = selector(initialResult, i);
+                yield return initialResult;
+            }
+        }
+
+        public static IEnumerable<string> JoinSelect<X>(this IEnumerable<X> sequence, string separator)
+        {
+            var iEnd = sequence.Count();
+            for (int i=0; i<iEnd; ++i)
+            {
+                yield return sequence.Take(i).Join(separator);
+            }
+        }
+
+        /// <summary>
+        /// Like Select, but silently ignores exceptions in f
+        /// </summary>
+        /// <typeparam name="X"></typeparam>
+        /// <typeparam name="Y"></typeparam>
+        /// <param name="x"></param>
+        /// <param name="f"></param>
+        /// <returns></returns>
+        public static IEnumerable<Y> SafeSelect<X, Y>(this IEnumerable<X> x, Func<X, Y> f)
+        {
+            foreach (var i in x)
+            {
+                Y y = default(Y);
+                bool ok = false;
+                try
+                {
+                    y = f(i);
+                    ok = true;
+                }
+                catch (Exception e)
+                {
+                    log.Warn(Sidi.Extensions.StringExtensions.SafeToString(i), e);
+                }
+
+                if (ok)
+                {
+                    yield return y;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Returns the maximum element as defined by f
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="S"></typeparam>
+        /// <param name="x"></param>
+        /// <param name="f"></param>
+        /// <returns></returns>
+        public static T Best<T, S>(this IEnumerable<T> x, Func<T, S> f) where S : IComparable<S>
+        {
+            var e = x.GetEnumerator();
+            e.MoveNext();
+            S max = f(e.Current);
+            T best = e.Current;
+            for (; e.MoveNext(); )
+            {
+                var s = f(e.Current);
+                if (max.CompareTo(s) < 0)
+                {
+                    max = s;
+                    best = e.Current;
+                }
+            }
+            return best;
+        }
+
+        public static IEnumerable<KeyValuePair<K, V>> Combine<K, V>(this IEnumerable<K> keys, IEnumerable<V> values)
+        {
+            var v = values.GetEnumerator();
+            foreach (var k in keys)
+            {
+                if (!v.MoveNext())
+                {
+                    break;
+                }
+                yield return new KeyValuePair<K, V>(k, v.Current);
+            }
+        }
+    }
+}
---
+++ tags/2.4.0.624/Sidi.Util/IMAP/ArgParser.cs
@@ -0,0 +1,295 @@
+// Copyright (c) 2012, Andreas Grimme (http://andreas-grimme.gmxhome.de/)
+// 
+// This file is part of sidi-util.
+// 
+// sidi-util is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// sidi-util is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with sidi-util. If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace Sidi.IMAP
+{
+    [CLSCompliant(false)]
+    public class ArgParser
+    {
+        public ArgParser(string argString)
+        : this(argString, null, null)
+        {
+        }
+
+        public ArgParser(string argString, TextReader input, TextWriter output)
+        {
+            this.input = input;
+            this.output = output;
+            this.argString = argString;
+        }
+
+        const string argSep = " ";
+        
+        public TextReader input;
+        TextWriter output;
+        string argString;
+        int begin = 0;
+
+        public string Rest
+        {
+            get
+            {
+                return argString.Substring(begin);
+            }
+        }
+            
+        public object GetArgument(ParameterInfo p)
+        {
+            return GetArgument(p.ParameterType);
+        }
+
+        public bool NextLine()
+        {
+            argString = input.ReadLine();
+            begin = 0;
+            return argString != null;
+        }
+
+        public object GetArgument(Type type)
+        {
+            if (type == typeof(ArgParser))
+            {
+                return this;
+            }
+            if (type == typeof(string))
+            {
+                if (argString[begin] == '"')
+                {
+                    return GetQuotedString();
+                }
+                else if (argString[begin] == '{')
+                {
+                    ++begin;
+                    var end = Index("}");
+                    var count = Int32.Parse(argString.Substring(begin, end - begin));
+                    CommandContinuationRequest();
+                    var b = new byte[count];
+                    ((StreamReader)input).BaseStream.Read(b, 0, count);
+                    argString = input.ReadLine();
+                    begin = 0;
+                    return System.Text.ASCIIEncoding.ASCII.GetString(b);
+                }
+                else
+                {
+                    return GetAtom();
+                }
+            }
+            else if (type == typeof(object[]))
+            {
+                object o = Get();
+                if (!(o is object[]))
+                {
+                    o = new object[] { o };
+                }
+                return o;
+            }
+            else
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public object Get()
+        {
+            if (IsNext("\""))
+            {
+                return GetQuotedString();
+            }
+            else if (IsNext("("))
+            {
+                return GetList();
+            }
+            else
+            {
+                return GetAtom();
+            }
+        }
+
+        const string quote = "\"";
+
+        Regex atom = new Regex(@"[^\(\)\{\ \}\[\]]+");
+
+        public void ArgSep()
+        {
+            Consume(argSep);
+        }
+
+        public object[] GetList()
+        {
+            Consume("(");
+            var elements = new List<object>();
+            while (!ConsumeIfNext(")"))
+            {
+                elements.Add(Get());
+                ConsumeIfNext(argSep);
+            }
+            return elements.ToArray();
+        }
+
+        public string GetQuotedString()
+        {
+            if (!ConsumeIfNext(quote))
+            {
+                throw new ParseException(this);
+            }
+
+            var end = Index(quote);
+            var s = Part(begin, end);
+            begin = end + quote.Length;
+            return s;
+        }
+
+        public int GetNumber()
+        {
+            return Int32.Parse(Get(new Regex(@"\d+")));
+        }
+
+        public int GetNumber(int length)
+        {
+            return Int32.Parse(Get(new Regex(String.Format(@"\d{{{0}}}", length))));
+        }
+
+        public UInt32 GetUInt32()
+        {
+            return System.UInt32.Parse(Get(new Regex(@"\d+")));
+        }
+
+        public string Get(Regex re)
+        {
+            var m = re.Match(argString, begin);
+            if (!m.Success || m.Index != begin)
+            {
+                throw new ParseException(this);
+            }
+            begin += m.Length;
+            return m.Groups[0].Value;
+        }
+
+        public bool IsNext(Regex re)
+        {
+            var m = re.Match(argString, begin);
+            return (m.Success && m.Index == begin);
+        }
+
+        public bool IsNext(string n)
+        {
+            if (begin <= argString.Length - n.Length)
+            {
+                if (argString.Substring(begin, n.Length).Equals(n, comparisonType))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void Consume(string next)
+        {
+            if (!ConsumeIfNext(next))
+            {
+                throw new ParseException(this);
+            }
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable")]
+        public class ParseException : Exception
+        {
+            public ParseException(ArgParser p)
+            {
+                ArgString = p.argString;
+                Index = p.begin;
+            }
+
+            public string ArgString;
+            public int Index;
+
+            public override string ToString()
+            {
+                return String.Format("{0}, {1}", ArgString, ArgString.Substring(Index));
+            }
+        }
+
+        int Index(string x)
+        {
+            var i = argString.IndexOf(x, begin, comparisonType);
+            if (i<0) { i = argString.Length; }
+            return i;
+        }
+
+        string Part(int begin, int end)
+        {
+            return argString.Substring(begin, end - begin);
+        }
+
+        StringComparison comparisonType = StringComparison.InvariantCultureIgnoreCase;
+
+        public bool ConsumeIfNext(string n)
+        {
+            if (begin <= argString.Length - n.Length)
+            {
+                if (argString.Substring(begin, n.Length).Equals(n, comparisonType))
+                {
+                    begin += n.Length;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool IsEnd
+        {
+            get
+            {
+                return !(begin < argString.Length);
+            }
+        }
+
+        public string GetAtom()
+        {
+            var m = atom.Match(argString, begin);
+            if (m.Success)
+            {
+                begin += m.Length;
+                return m.Groups[0].Value;
+            }
+            else
+            {
+                throw new ParseException(this);
+            }
+        }
+
+        void CommandContinuationRequest()
+        {
+            output.WriteLine("+");
+        }
+
+        static Regex optionalWhitespace = new Regex(@"\s*");
+
+        internal void SkipSpace()
+        {
+            Get(optionalWhitespace);
+        }
+    }
+}
