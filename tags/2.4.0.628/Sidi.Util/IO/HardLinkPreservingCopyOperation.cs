9aba56b8f5d0155b6195ef4e3246c850e5767e74
andreas.grimme@googlemail.com <andreas.grimme@googlemail.com@8a84ef9e-818e-11dd-b2bb-736fb4d8a922>
2013-01-05 06:16:45 +0000

    Automatic build of sidi-util

---
+++ tags/2.4.0.628/Sidi.Util/IO/HardLinkPreservingCopyOperation.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sidi.IO
+{
+    /// <summary>
+    /// Copies files from one volume to another while preserving hardlinks
+    /// This means that is two files on the source volume are hard-linked,
+    /// the destination files will be hard-linked together as well.
+    /// </summary>
+    public class HardLinkPreservingCopyOperation
+    {
+        public void Copy(LPath source, LPath destination)
+        {
+            if (source.IsDirectory)
+            {
+                var children = source.Children;
+                if (children.Count > 0)
+                {
+                    destination.EnsureDirectoryExists();
+                    foreach (var c in source.Children)
+                    {
+                        Copy(c, destination.CatDir(c.FileName));
+                    }
+                }
+            }
+            else
+            {
+                var fi = source.Info.FileIndex;
+                LPath existingDestinationFile;
+                if (sourceFileIdToDestinationPath.TryGetValue(fi, out existingDestinationFile))
+                {
+                    // re-create hard link
+                    
+                    LFile.CreateHardLink(destination, existingDestinationFile);
+                }
+                else
+                {
+                    LFile.Copy(source, destination);
+                    sourceFileIdToDestinationPath[fi] = destination;
+                }
+            }
+        }
+
+        Dictionary<ulong, LPath> sourceFileIdToDestinationPath = new Dictionary<ulong, LPath>();
+    }
+}
