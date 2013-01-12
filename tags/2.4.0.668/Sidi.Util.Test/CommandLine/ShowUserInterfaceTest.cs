0b916142aebcd45f775069caedd74111d8c3adc6
andreas.grimme@googlemail.com <andreas.grimme@googlemail.com@8a84ef9e-818e-11dd-b2bb-736fb4d8a922>
2013-01-12 04:40:35 +0000

    Automatic build of sidi-util

---
+++ tags/2.4.0.668/Sidi.Util.Test/CommandLine/ShowUserInterfaceTest.cs
@@ -0,0 +1,60 @@
+// Copyright (c) 2009, Andreas Grimme (http://andreas-grimme.gmxhome.de/)
+// 
+// This file is part of sidi-util.
+// 
+// sidi-util is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// sidi-util is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with sidi-util. If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Sidi.CommandLine.Test
+{
+    [TestFixture]
+    class ShowUserInterfaceTest
+    {
+        [Test, Explicit("interactive")]
+        public void UserInterface()
+        {
+            var p = ParserTest.ParserWithAllTestApps();
+            p.Parse(new string[] { "ui" });
+        }
+
+        [Test, Explicit("UI")]
+        public void ActionControl()
+        {
+            var ui = new ShowUserInterface(null);
+            var p = ParserTest.ParserWithAllTestApps();
+            var c = ui.ToDialog(p.GetAction("Greet"));
+            c.ShowAndExecute();
+        }
+
+        [Test, Explicit("ui")]
+        public void UI2()
+        {
+            var p = ParserTest.ParserWithAllTestApps();
+            new ShowUserInterface2(p).UserInterface();
+        }
+
+        [Test, Explicit("UI")]
+        public void SubCommandUi()
+        {
+            var a = new Sidi.CommandLine.Test.ParserTest.TestAppWithStringList();
+            var p = new Parser(a);
+            new ShowUserInterface(p).UserInterface();
+        }
+    }
+}
