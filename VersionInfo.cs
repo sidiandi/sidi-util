0a204a164df19b55983465077af6e44eec23391e
andreas.grimme@gmail.com <andreas.grimme@gmail.com@8a84ef9e-818e-11dd-b2bb-736fb4d8a922>
2013-01-29 14:14:19 +0000

    Automatic build of sidi-util

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright (c) Andreas Grimme 2010")]
 [assembly: AssemblyCompany("sidi")]
 [assembly: AssemblyProduct("sidi-util")]
-[assembly: AssemblyVersion("2.4.0.673")]
-[assembly: AssemblyFileVersion("2.4.0.673")]
+[assembly: AssemblyVersion("2.4.0.674")]
+[assembly: AssemblyFileVersion("2.4.0.674")]
 
 
