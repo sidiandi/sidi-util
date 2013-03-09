9b4298f6077e2a235741052f1150b036160ece7d
andreas.grimme@googlemail.com <andreas.grimme@googlemail.com@8a84ef9e-818e-11dd-b2bb-736fb4d8a922>
2013-03-09 08:59:45 +0000

    Automatic build of sidi-util

---
+++ tags/2.4.0.697/Sidi.Util.Test/Cache/CacheTest.cs
@@ -0,0 +1,113 @@
+// Copyright (c) 2009, Andreas Grimme (http://andreas-grimme.gmxhome.de/)
+// 
+// This file is part of sidi-util.
+// 
+// sidi-util is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// sidi-util is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with sidi-util. If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using System.Reflection;
+
+namespace Sidi.Cache
+{
+    [TestFixture]
+    public class CacheTest
+    {
+        [Test]
+        public void DoCache()
+        {
+            var c = Cache.Local(MethodBase.GetCurrentMethod());
+            c.Clear();
+            
+            for (int i = 0; i < 10; ++i)
+            {
+                var result = c.GetCached(i, () => i * i);
+                Assert.AreEqual(i * i, result);
+            }
+
+            c = Cache.Local(MethodBase.GetCurrentMethod());
+            for (int i = 0; i < 10; ++i)
+            {
+                Assert.IsTrue(c.IsCached(i));
+            }
+
+            c.Clear();
+            for (int i = 0; i < 10; ++i)
+            {
+                Assert.IsFalse(c.IsCached(i));
+            }
+        }
+
+        [Test]
+        public void ExceptionRemembered()
+        {
+            var c = Cache.Local(MethodBase.GetCurrentMethod());
+            c.Clear();
+            c.RememberExceptions = true;
+
+            var d = 0;
+            try
+            {
+                var r = c.GetCached(d, () => 1 / d);
+                Assert.IsTrue(false);
+            }
+            catch (Exception e)
+            {
+                Assert.IsTrue(e is System.DivideByZeroException);
+            }
+
+            try
+            {
+                // this should not raise an exception
+                // but since RememberExceptions = true, 
+                // the cached DivideByZeroException will be returned.
+                var r = c.GetCached(d, () => 1 / 1); 
+                Assert.IsTrue(false);
+            }
+            catch (Exception e)
+            {
+                Assert.IsTrue(e is System.DivideByZeroException);
+            }
+        }
+
+        [Test]
+        public void ExceptionNotRemembered()
+        {
+            var c = Cache.Local(MethodBase.GetCurrentMethod());
+            c.Clear();
+            c.RememberExceptions = false;
+
+            var d = 0;
+            try
+            {
+                var r = c.GetCached(d, () => 1 / d);
+                Assert.IsTrue(false);
+            }
+            catch (Exception e)
+            {
+                Assert.IsTrue(e is System.DivideByZeroException);
+            }
+
+            // this should not raise an exception
+            // and since RememberExceptions = true, 
+            // the cached DivideByZeroException will not be returned.
+            {
+                var r = c.GetCached(d, () => 1 / 1);
+            }
+        }
+    }
+}
---
+++ tags/2.4.0.697/Sidi.Util/Util/TimeIntervalParser.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Sidi.CommandLine;
+
+namespace Sidi.Util
+{
+    [Usage("Parse TimeInterval string representations")]
+    public class TimeIntervalParser
+    {
+        public static TimeInterval Parse(string value)
+        {
+            var tip = new TimeIntervalParser();
+            var p = new Parser(tip);
+            p.Parse(Tokenizer.ToArray(value));
+            return tip.Value;
+        }
+
+        public TimeIntervalParser()
+        {
+            Value = new TimeInterval(DateTime.Now, TimeSpan.Zero);
+        }
+
+        public TimeInterval Value { get; set; }
+
+        [Usage("Current day as time interval")]
+        public void Today()
+        {
+            Value = TimeInterval.Today;
+        }
+
+        [Usage("Current year")]
+        public void CurrentYear()
+        {
+            Value = TimeInterval.Year(DateTime.Now);
+        }
+
+        [Usage("Year")]
+        public void Year(int year)
+        {
+            Value = TimeInterval.Year(new DateTime(year, 1, 1));
+        }
+
+        [Usage("Specify begin time")]
+        public void From(DateTime time)
+        {
+            Value = new TimeInterval(time, Value.End);
+        }
+
+        [Usage("Specify end time")]
+        public void To(DateTime time)
+        {
+            Value = new TimeInterval(Value.Begin, time);
+        }
+    }
+}
