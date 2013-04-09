f206e9a01fb016d568e075bacf622feba5902977
andreas.grimme@googlemail.com <andreas.grimme@googlemail.com@8a84ef9e-818e-11dd-b2bb-736fb4d8a922>
2013-04-09 04:13:23 +0000

    Automatic build of sidi-util

---
+++ tags/2.4.0.734/Sidi.Util.Test/Util/Base32Test.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Sidi.Util
+{
+    [TestFixture]
+    public class Base32Test : TestBase
+    {
+        [Test]
+        public void Encode()
+        {
+            BASE32("foobar", "MZXW6YTBOI");
+            BASE32("foo", "MZXW6");
+            BASE32("foob", "MZXW6YQ");
+            BASE32("fooba", "MZXW6YTB");
+        }
+
+        void BASE32(string input, string encoded)
+        {
+            Assert.AreEqual(encoded, Base32.Encode(ASCIIEncoding.ASCII.GetBytes(input)));
+        }
+    }
+}
