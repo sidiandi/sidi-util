539327947db8e80dc075807bf78fbe73cb91e58f
andreas.grimme@googlemail.com <andreas.grimme@googlemail.com@8a84ef9e-818e-11dd-b2bb-736fb4d8a922>
2013-04-12 01:57:00 +0000

    Automatic build of sidi-util

---
+++ tags/2.4.0.741/Sidi.Util/Persistence/SharedConnection.cs
@@ -0,0 +1,143 @@
+// Copyright (c) 2012, Andreas Grimme (http://andreas-grimme.gmxhome.de/)
+// 
+// This file is part of sidi-util.
+// 
+// sidi-util is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// sidi-util is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with sidi-util. If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SQLite;
+using System.Threading;
+using System.IO;
+
+namespace Sidi.Persistence
+{
+    public class SharedConnection : IDisposable
+    {
+        public SharedConnection(SQLiteConnection c)
+        {
+            shared = new Shared(c);
+            shared.AddRef();
+        }
+
+        public SharedConnection(SharedConnection c)
+        {
+            c.shared.AddRef();
+            shared = c.shared;
+        }
+
+        public SQLiteConnection Connection
+        {
+            get { return shared.connection; }
+        }
+
+        class Shared : IDisposable
+        {
+            public SQLiteConnection connection;
+            int references = 0;
+
+            public Shared(SQLiteConnection c)
+            {
+                connection = c;
+            }
+
+            public void AddRef()
+            {
+                lock (this)
+                {
+                    ++references;
+                }
+            }
+
+            public bool RemoveRef()
+            {
+                lock (this)
+                {
+                    --references;
+                    return references > 0;
+                }
+            }
+
+            public void Dispose()
+            {
+                Close();
+            }
+
+            void Close()
+            {
+                connection.Dispose();
+                /*
+                var csb = new SQLiteConnectionStringBuilder(connection.ConnectionString);
+                var dbFile = csb.DataSource;
+                connection.Dispose();
+                GC.Collect();
+                for (int i = 0; i < 10; ++i)
+                {
+                    try
+                    {
+                        using (var f = File.OpenWrite(dbFile))
+                        {
+                        }
+                        break;
+                    }
+                    catch
+                    {
+                    }
+                    Thread.Sleep(100);
+                }
+                 */
+            }
+        }
+        Shared shared;
+
+
+
+        private bool disposed = false;
+
+        //Implement IDisposable.
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    if (!shared.RemoveRef())
+                    {
+                        shared.Dispose();
+                    }
+
+                }
+                // Free your own state (unmanaged objects).
+                // Set large fields to null.
+                disposed = true;
+            }
+        }
+
+        // Use C# destructor syntax for finalization code.
+        ~SharedConnection()
+        {
+            // Simply call Dispose(false).
+            Dispose(false);
+        }
+
+    }
+}
