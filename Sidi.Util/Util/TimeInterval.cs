fd2e3b6ec089749951f3e65d31fd0ee38e16fe7e
andreas.grimme@googlemail.com <andreas.grimme@googlemail.com@8a84ef9e-818e-11dd-b2bb-736fb4d8a922>
2013-05-14 07:51:00 +0000

    Automatic build of sidi-util

--- Sidi.Util/Util/TimeInterval.cs
+++ Sidi.Util/Util/TimeInterval.cs
@@ -6,6 +6,7 @@ using System.Text.RegularExpressions;
 
 namespace Sidi.Util
 {
+    [Serializable]
     public class TimeInterval
     {
         public TimeInterval(DateTime begin, TimeSpan duration)
