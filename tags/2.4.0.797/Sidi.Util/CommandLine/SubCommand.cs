c9a226c89f1bbc817290c1472f948eb72a2d040b
andreas.grimme@googlemail.com <andreas.grimme@googlemail.com@8a84ef9e-818e-11dd-b2bb-736fb4d8a922>
2013-06-26 04:20:37 +0000

    Automatic build of sidi-util

---
+++ tags/2.4.0.797/Sidi.Util/CommandLine/SubCommand.cs
@@ -0,0 +1,256 @@
+// Copyright (c) 2012, Andreas Grimme (http://andreas-grimme.gmxhome.de/)
+// 
+// This file is part of sidi-util.
+// 
+// sidi-util is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// sidi-util is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with sidi-util. If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.ComponentModel;
+using System.IO;
+using Sidi.Util;
+using Sidi.Extensions;
+
+namespace Sidi.CommandLine
+{
+    public class SubCommand : IParserItem
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public SubCommand(object application, MemberInfo memberInfo)
+        {
+            Application = application;
+            MemberInfo = memberInfo;
+        }
+
+        public object Application { get; private set; }
+        public MemberInfo MemberInfo { get; private set; }
+
+        public string Name { get { return MemberInfo.Name; } }
+        public string Usage
+        {
+            get
+            {
+                var u = Sidi.CommandLine.Usage.Get(MemberInfo);
+                if (u != null)
+                {
+                    return u;
+                }
+                u = Sidi.CommandLine.Usage.Get(Type);
+                if (u != null)
+                {
+                    return u;
+                }
+                return String.Empty;
+            }
+        }
+
+        public IEnumerable<string> Categories
+        {
+            get
+            {
+                var c = MemberInfo.GetCustomAttributes(typeof(CategoryAttribute), true)
+                    .Select(x => ((CategoryAttribute)x).Category);
+                return c.Any() ? c : new string[] { String.Empty };
+            }
+        }
+
+        public string Syntax
+        {
+            get
+            {
+                return this.Name + " [subcommands...] ;";
+            }
+        }
+
+        public object GetValue()
+        {
+            MemberInfo i = MemberInfo;
+            if (i.MemberType == MemberTypes.Field)
+            {
+                FieldInfo fieldInfo = (FieldInfo)i;
+                return fieldInfo.GetValue(Application);
+            }
+            else if (i.MemberType == MemberTypes.Property)
+            {
+                PropertyInfo propertyInfo = (PropertyInfo)i;
+                return propertyInfo.GetValue(Application, new object[] { });
+            }
+            throw new InvalidDataException(i.MemberType.ToString());
+        }
+
+        public Type Type
+        {
+            get
+            {
+                MemberInfo i = MemberInfo;
+                if (i.MemberType == MemberTypes.Field)
+                {
+                    FieldInfo fieldInfo = (FieldInfo)i;
+                    return fieldInfo.FieldType;
+                }
+                else if (i.MemberType == MemberTypes.Property)
+                {
+                    PropertyInfo propertyInfo = (PropertyInfo)i;
+                    return propertyInfo.PropertyType;
+                }
+                throw new InvalidOperationException(i.MemberType.ToString());
+            }
+        }
+
+        public void PrintScriptFileSample(TextWriter w)
+        {
+            w.Write("# "); w.WriteLine(Usage);
+            w.Write("# "); w.WriteLine(Syntax);
+            w.WriteLine();
+        }
+
+        public override string ToString()
+        {
+            return String.Format("Subcommand {0}", Name);
+        }
+
+        public object Handle(IList<string> args, bool execute)
+        {
+            var parser = new Parser();
+            parser.Applications.AddRange(new object[]
+                {
+                    CommandInstance,
+                    new ShowHelp(parser),
+                    new ShowUserInterface(parser),
+                    new ShowWebServer(parser)
+                });
+            parser.Applications.Add(new LogOptions());
+
+            log.InfoFormat("{0}", this);
+            if (args.Any())
+            {
+                while (args.Any())
+                {
+                    if (args.First() == Parser.ListTerminator)
+                    {
+                        args.RemoveAt(0);
+                        break;
+                    }
+
+                    if (execute)
+                    {
+                        parser.ParseSingleCommand(args);
+                    }
+                    else
+                    {
+                        parser.CheckSingleCommand(args);
+                    }
+                }
+            }
+            else
+            {
+                parser.ShowUsage();
+            }
+            return null;
+        }
+
+        public bool IsInstanciated
+        {
+            get
+            {
+                return GetValue() != null;
+            }
+        }
+
+        void Instanciate()
+        {
+            object instance = null;
+            if (MemberInfo is FieldInfo)
+            {
+                var fi = (FieldInfo)MemberInfo;
+                instance = Activator.CreateInstance(fi.FieldType);
+                fi.SetValue(Application, instance);
+            }
+            else if (MemberInfo is PropertyInfo)
+            {
+                var pi = (PropertyInfo)MemberInfo;
+                instance = Activator.CreateInstance(pi.PropertyType);
+                pi.SetValue(Application, instance, new object[] { });
+            }
+        }
+
+        public object ProvideCommandInstance()
+        {
+            var o = CommandInstance;
+            if (o == null)
+            {
+                throw new Exception();
+            }
+            return o;
+        }
+
+        public object CommandInstance
+        {
+            get
+            {
+                var instance = GetValue();
+                if (instance == null)
+                {
+                    Instanciate();
+                    return CommandInstance;
+                }
+                else
+                {
+                    return instance;
+                }
+            }
+        }
+
+        public string UsageText
+        {
+            get
+            {
+                string indent = "  ";
+                return String.Format(
+                    Parser.CultureInfo,
+                    @"{0}
+{1}",
+                    Syntax,
+                    Usage.Indent(indent)
+                    );
+            }
+        }
+
+        public bool IsPersistent
+        {
+            get
+            {
+                return MemberInfo.GetCustomAttributes(typeof(PersistentAttribute), true).Any();
+            }
+        }
+
+        public bool IsPassword
+        {
+            get
+            {
+                return MemberInfo.GetCustomAttributes(typeof(PasswordAttribute), true).Any();
+            }
+        }
+
+        public static bool IsSubCommand(MemberInfo mi)
+        {
+            return mi.GetCustomAttributes(typeof(SubCommandAttribute), true).Any();
+        }
+    }
+
+}
---
+++ tags/2.4.0.797/Sidi.Util/Forms/ChooseDialog.designer.cs
@@ -0,0 +1,124 @@
+// Copyright (c) 2012, Andreas Grimme (http://andreas-grimme.gmxhome.de/)
+// 
+// This file is part of sidi-util.
+// 
+// sidi-util is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// sidi-util is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with sidi-util. If not, see <http://www.gnu.org/licenses/>.
+
+namespace Sidi.Forms
+{
+    partial class ChooseDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ChooseDialog));
+            this.listViewObjects = new Sidi.Forms.GenericListView();
+            this.columnHeaderPath = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.buttonOK = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listViewObjects
+            // 
+            this.listViewObjects.AllowDrop = true;
+            this.listViewObjects.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listViewObjects.FullRowSelect = true;
+            this.listViewObjects.Items = ((System.Collections.IList)(resources.GetObject("listViewObjects.Items")));
+            this.listViewObjects.Location = new System.Drawing.Point(12, 12);
+            this.listViewObjects.MultiSelect = false;
+            this.listViewObjects.Name = "listViewObjects";
+            this.listViewObjects.Size = new System.Drawing.Size(903, 590);
+            this.listViewObjects.TabIndex = 14;
+            this.listViewObjects.UseCompatibleStateImageBehavior = false;
+            this.listViewObjects.View = System.Windows.Forms.View.Details;
+            this.listViewObjects.VirtualMode = true;
+            this.listViewObjects.ItemActivate += new System.EventHandler(this.listViewObjects_ItemActivate);
+            // 
+            // columnHeaderPath
+            // 
+            this.columnHeaderPath.Text = "Path";
+            this.columnHeaderPath.Width = 780;
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonCancel.Location = new System.Drawing.Point(93, 608);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.TabIndex = 16;
+            this.buttonCancel.Text = "&Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            // 
+            // buttonOK
+            // 
+            this.buttonOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonOK.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.buttonOK.Location = new System.Drawing.Point(12, 608);
+            this.buttonOK.Name = "buttonOK";
+            this.buttonOK.Size = new System.Drawing.Size(75, 23);
+            this.buttonOK.TabIndex = 15;
+            this.buttonOK.Text = "&OK";
+            this.buttonOK.UseVisualStyleBackColor = true;
+            // 
+            // ChooseDialog
+            // 
+            this.AcceptButton = this.buttonOK;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.buttonCancel;
+            this.ClientSize = new System.Drawing.Size(927, 643);
+            this.Controls.Add(this.listViewObjects);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonOK);
+            this.Name = "ChooseDialog";
+            this.Text = "Choose One";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public GenericListView listViewObjects;
+        private System.Windows.Forms.ColumnHeader columnHeaderPath;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.Button buttonOK;
+
+    }
+}
