6e006d774bacc27ea42ef854341878fa0b7efcec
andreas.grimme@googlemail.com <andreas.grimme@googlemail.com@8a84ef9e-818e-11dd-b2bb-736fb4d8a922>
2013-07-10 03:43:55 +0000

    Automatic build of sidi-util

---
+++ tags/2.4.0.818/Sidi.Util/CommandLine/ShowHelp.cs
@@ -0,0 +1,67 @@
+// Copyright (c) 2012, Andreas Grimme (http://andreas-grimme.gmxhome.de/)
+// 
+// This file is part of sidi-util.
+// 
+// sidi-util is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// sidi-util is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with sidi-util. If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.ComponentModel;
+using Sidi.Util;
+
+namespace Sidi.CommandLine
+{
+    public class ShowHelp
+    {
+        Parser parser;
+
+        public ShowHelp(Parser parser)
+        {
+            this.parser = parser;
+        }
+
+        [Usage("Opens the manual")]
+        [Category(Parser.categoryUsage)]
+        public void Manual()
+        {
+            Sidi.CommandLine.Manual.Show(parser);
+        }
+
+        [Usage("Print detailed usage message")]
+        [Category(Parser.categoryUsage)]
+        public void Usage()
+        {
+            parser.WriteUsage(Console.Out);
+        }
+
+        [Usage("Shows help for all options and actions that match pattern")]
+        [Category(Parser.categoryUsage)]
+        public void Help(RegexIgnoreCase pattern = null)
+        {
+            if (pattern == null)
+            {
+                pattern = new RegexIgnoreCase(String.Empty);
+            }
+
+            parser.WriteUsageByCategory(
+                Console.Out,
+                parser.Items
+                .Where(i => pattern.IsMatch(i.UsageText))
+                );
+        }
+    }
+}
