841efae6b22fc59f983b5ca981a918c1924fa737
andreas.grimme@googlemail.com <andreas.grimme@googlemail.com@8a84ef9e-818e-11dd-b2bb-736fb4d8a922>
2013-07-15 03:50:08 +0000

    Automatic build of sidi-util

--- trunk/Sidi.Util.Test/IO/File.cs
+++ trunk/Sidi.Util.Test/IO/File.cs
@@ -26,192 +26,194 @@ using Sidi.Test;
 
 namespace Sidi.IO
 {
-        [TestFixture]
-        public class FileTest : TestBase
+    [TestFixture]
+    public class FileTest : TestBase
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public LPath root;
+        public LPath lp;
+
+        [SetUp]
+        public void Setup()
         {
-            private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+            root = Paths.BinDir.CatDir(@"test\LongNameTest");
 
-            public LPath root;
-            public LPath lp;
+            root.EnsureNotExists();
 
-            [SetUp]
-            public void Setup()
+            lp = root.CatDir(
+                Enumerable.Range(0, 100)
+                    .Select(x => String.Format("PathPart{0}", x)));
+
+            lp.EnsureParentDirectoryExists();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            root.EnsureNotExists();
+            Assert.IsFalse(root.Exists);
+        }
+
+        [Test]
+        public void SetupAndTearDown()
+        {
+            log.Info("do nothing");
+        }
+
+        [Test]
+        public void Enum()
+        {
+            // create 10 files
+            foreach (var i in Enumerable.Range(0, 10))
             {
-                root = Paths.BinDir.CatDir(@"test\LongNameTest");
-
-                root.EnsureNotExists();
-
-                lp = root.CatDir(
-                    Enumerable.Range(0, 100)
-                        .Select(x => String.Format("PathPart{0}", x)));
-
-                lp.EnsureParentDirectoryExists();
+                CreateSampleFile(root.CatDir(i.ToString()));
             }
 
-            [TearDown]
-            public void TearDown()
-            {
-                root.EnsureNotExists();
-                Assert.IsFalse(root.Exists);
-            }
+            var e = new LFileSystemInfo(root).GetChildren();
+            Assert.IsTrue(e.Count() >= 10);
+        }
 
-            [Test]
-            public void SetupAndTearDown()
+        public void CreateSampleFile(LPath lp)
+        {
+            using (var f = LFile.Open(lp, System.IO.FileMode.Create))
             {
-                log.Info("do nothing");
-            }
-
-            [Test]
-            public void Enum()
-            {
-                // create 10 files
-                foreach (var i in Enumerable.Range(0, 10))
+                using (var o = new System.IO.StreamWriter(f))
                 {
-                    CreateSampleFile(root.CatDir(i.ToString()));
-                }
-
-                var e = new LFileSystemInfo(root).GetChildren();
-                Assert.IsTrue(e.Count() >= 10);
-            }
-
-            public void CreateSampleFile(LPath lp)
-            {
-                using (var f = LFile.Open(lp, System.IO.FileMode.Create))
-                {
-                    using (var o = new System.IO.StreamWriter(f))
-                    {
-                        o.WriteLine("Hello");
-                    }
+                    o.WriteLine("Hello");
                 }
             }
+        }
 
-            [Test]
-            public void Copy()
-            {
-                CreateSampleFile(lp);
-                var lpCopy = lp.CatName(".copy");
-                LFile.Copy(lp, lpCopy);
-                Assert.IsTrue(LFile.Exists(lpCopy));
-                log.Info(lp.Parent.Children);
-                log.Info(lp);
-            }
+        [Test]
+        public void Copy()
+        {
+            CreateSampleFile(lp);
+            var lpCopy = lp.CatName(".copy");
+            LFile.Copy(lp, lpCopy);
+            Assert.IsTrue(LFile.Exists(lpCopy));
+            log.Info(lp.Parent.Children);
+            log.Info(lp);
+        }
 
-            [Test]
-            public void CopyProgress()
+        [Test]
+        public void CopyProgress()
+        {
+            var bigSampleFile = root.CatDir("big");
+
+            using (var f = LFile.Open(bigSampleFile, System.IO.FileMode.Create))
             {
-                var bigSampleFile = root.CatDir("big");
-                
-                using (var f = LFile.Open(bigSampleFile, System.IO.FileMode.Create))
+                var b = new byte[1024 * 1024];
+                for (int i = 0; i < 100; ++i)
                 {
-                    var b = new byte[1024*1024];
-                    for (int i = 0; i < 100; ++i)
-                    {
-                        f.Write(b, 0, b.Length);
-                    }
+                    f.Write(b, 0, b.Length);
                 }
-
-                log.Info(bigSampleFile.Info.Length);
-                var lpCopy = bigSampleFile.CatName(".copy");
-                LFile.Copy(bigSampleFile, lpCopy, true, (p) =>
-                {
-                    log.Info(p.Message);
-                });
-                Assert.IsTrue(LFile.Exists(lpCopy));
-                lpCopy.EnsureNotExists();
-                bigSampleFile.EnsureNotExists();
             }
 
-            [Test]
-            public void HardLink()
+            log.Info(bigSampleFile.Info.Length);
+            var lpCopy = bigSampleFile.CatName(".copy");
+            LFile.Copy(bigSampleFile, lpCopy, true, (p) =>
             {
-                CreateSampleFile(lp);
-                var lpCopy = lp.CatName(".link");
-                LFile.CreateHardLink(lpCopy, lp);
-                Assert.IsTrue(LFile.Exists(lpCopy));
-                log.Info(lp.Parent.Children);
-                log.Info(lp);
-                Assert.IsTrue(LFile.EqualByTimeAndLength(lp, lpCopy));
+                log.Info(p.Message);
+            });
+            Assert.IsTrue(LFile.Exists(lpCopy));
+            lpCopy.EnsureNotExists();
+            bigSampleFile.EnsureNotExists();
+        }
+
+        [Test]
+        public void HardLink()
+        {
+            CreateSampleFile(lp);
+            var lpCopy = lp.CatName(".link");
+            LFile.CreateHardLink(lpCopy, lp);
+            Assert.IsTrue(LFile.Exists(lpCopy));
+            log.Info(lp.Parent.Children);
+            log.Info(lp);
+            Assert.IsTrue(LFile.EqualByTimeAndLength(lp, lpCopy));
+        }
+
+        [Test]
+        public void Move()
+        {
+            CreateSampleFile(lp);
+            var m = new LPath(lp.Parts.Take(20));
+            var dest = root.CatDir("moved");
+            LDirectory.Move(m, dest);
+            Assert.IsTrue(LDirectory.Exists(dest));
+            Assert.IsFalse(LFile.Exists(lp));
+            Assert.IsTrue(LDirectory.Exists(root));
+        }
+
+        [Test]
+        public void DeleteReadOnlyFiles()
+        {
+            CreateSampleFile(lp);
+            var info = new LFileSystemInfo(lp);
+            Assert.IsFalse(info.IsReadOnly);
+            info.IsReadOnly = true;
+            Assert.IsTrue(info.IsReadOnly);
+            LFile.Delete(lp);
+        }
+
+        [Test]
+        public void Equal()
+        {
+            var f1 = lp;
+            CreateSampleFile(f1);
+            var f2 = f1.CatName(".copy");
+            LFile.Copy(f1, f2);
+            Assert.IsTrue(LFile.EqualByContent(f1, f2));
+
+            using (var s = LFile.Open(f2, System.IO.FileMode.Create))
+            {
+                s.WriteByte(0);
             }
 
-            [Test]
-            public void Move()
+            Assert.IsFalse(LFile.EqualByContent(f1, f2));
+        }
+
+        [Test]
+        public void WriteAllText()
+        {
+            var d = this.TestFile("Long").CatDir("blablabla");
+            d.EnsureNotExists();
+            var p = d.CatDir(".moviesidi");
+            LFile.WriteAllText(p, "hello");
+            Assert.AreEqual("hello", LFile.ReadAllText(p));
+            d.EnsureNotExists();
+        }
+
+        [Test]
+        public void Open()
+        {
+            var text = "world";
+            var d = this.TestFile("Long").CatDir("234234", "23443", "blablabla");
+            using (var s = LFile.StreamWriter(d))
             {
-                CreateSampleFile(lp);
-                var m = new LPath(lp.Parts.Take(20));
-                var dest = root.CatDir("moved");
-                LDirectory.Move(m, dest);
-                Assert.IsTrue(LDirectory.Exists(dest));
-                Assert.IsFalse(LFile.Exists(lp));
-                Assert.IsTrue(LDirectory.Exists(root));
+                s.WriteLine("hello");
             }
-
-            [Test]
-            public void DeleteReadOnlyFiles()
+            using (var s = LFile.StreamWriter(d))
             {
-                CreateSampleFile(lp);
-                var info = new LFileSystemInfo(lp);
-                Assert.IsFalse(info.IsReadOnly);
-                info.IsReadOnly = true;
-                Assert.IsTrue(info.IsReadOnly);
-                LFile.Delete(lp);
+                s.Write(text);
             }
-
-            [Test]
-            public void Equal()
+            using (var s = LFile.StreamReader(d))
             {
-                var f1 = lp;
-                CreateSampleFile(f1);
-                var f2 = f1.CatName(".copy");
-                LFile.Copy(f1, f2);
-                Assert.IsTrue(LFile.EqualByContent(f1, f2));
-
-                using (var s = LFile.Open(f2, System.IO.FileMode.Create))
-                {
-                    s.WriteByte(0);
-                }
-
-                Assert.IsFalse(LFile.EqualByContent(f1, f2));
+                Assert.AreEqual(text, s.ReadToEnd());
             }
+            Assert.AreEqual(text, LFile.ReadAllText(d));
+        }
 
-            [Test]
-            public void WriteAllText()
+        [Test]
+        public void Reopen()
+        {
+            var random = new Random();
+            var relPath = new LPath(Enumerable.Range(0, 100)
+                .Select(x => random.String(10)));
+            var rootDir = TestFile("opentest");
+            rootDir.EnsureNotExists();
+            try
             {
-                var d = this.TestFile("Long").CatDir("blablabla");
-                d.EnsureNotExists();
-                var p = d.CatDir(".moviesidi");
-                LFile.WriteAllText(p, "hello");
-                Assert.AreEqual("hello", LFile.ReadAllText(p));
-                d.EnsureNotExists();
-            }
-
-            [Test]
-            public void Open()
-            {
-                var text = "world";
-                var d = this.TestFile("Long").CatDir("234234", "23443", "blablabla");
-                using (var s = LFile.StreamWriter(d))
-                {
-                    s.WriteLine("hello");
-                }
-                using (var s = LFile.StreamWriter(d))
-                {
-                    s.Write(text);
-                }
-                using (var s = LFile.StreamReader(d))
-                {
-                    Assert.AreEqual(text, s.ReadToEnd());
-                }
-                Assert.AreEqual(text, LFile.ReadAllText(d));
-            }
-
-            [Test]
-            public void Reopen()
-            {
-                var random = new Random();
-                var relPath = new LPath(Enumerable.Range(0, 100)
-                    .Select(x => random.String(10)));
-                var rootDir = TestFile("opentest");
-                rootDir.EnsureNotExists();
                 var f = rootDir.CatDir(relPath);
                 var text = "Hello, world!";
                 using (var w = f.WriteText())
@@ -226,25 +228,30 @@ namespace Sidi.IO
                 f.EnsureNotExists();
                 log.Info(f);
             }
-
-            [Test]
-            public void CannotDelete()
+            finally
             {
-                var p = TestFile("delete_me");
-                using (var w = LFile.StreamWriter(p))
+                rootDir.EnsureNotExists();
+            }
+        }
+
+        [Test]
+        public void CannotDelete()
+        {
+            var p = TestFile("delete_me");
+            using (var w = LFile.StreamWriter(p))
+            {
+                try
                 {
-                    try
-                    {
-                        p.EnsureNotExists();
-                        Assert.IsTrue(false);
-                    }
-                    catch (System.IO.IOException ex)
-                    {
-                        log.Info(ex.ToString());
-                        var w32 = (Win32Exception)ex.InnerException;
-                        Assert.IsFalse(w32.Message.Contains("The operation completed successfully"));
-                    }
+                    p.EnsureNotExists();
+                    Assert.IsTrue(false);
+                }
+                catch (System.IO.IOException ex)
+                {
+                    log.Info(ex.ToString());
+                    var w32 = (Win32Exception)ex.InnerException;
+                    Assert.IsFalse(w32.Message.Contains("The operation completed successfully"));
                 }
             }
         }
+    }
 }
