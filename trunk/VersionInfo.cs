babf69f58efca23edff8877b8b33adc79010a5e5
andreas.grimme@googlemail.com <andreas.grimme@googlemail.com@8a84ef9e-818e-11dd-b2bb-736fb4d8a922>
2013-08-04 08:23:20 +0000

    Automatic build of sidi-util

--- trunk/VersionInfo.cs
+++ trunk/VersionInfo.cs
@@ -11,7 +11,7 @@
 [assembly: System.Reflection.AssemblyCopyright("Copyright (c) Andreas Grimme 2010")]
 [assembly: System.Reflection.AssemblyCompany("sidi")]
 [assembly: System.Reflection.AssemblyProduct("sidi-util")]
-[assembly: System.Reflection.AssemblyVersion("2.4.0.871")]
-[assembly: System.Reflection.AssemblyFileVersion("2.4.0.871")]
+[assembly: System.Reflection.AssemblyVersion("2.4.0.873")]
+[assembly: System.Reflection.AssemblyFileVersion("2.4.0.873")]
 
 
