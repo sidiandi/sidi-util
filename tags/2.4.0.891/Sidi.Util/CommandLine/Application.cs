652598d7dc5e3e6bdf32f215d5e251cfb099e00b
andreas.grimme@googlemail.com <andreas.grimme@googlemail.com@8a84ef9e-818e-11dd-b2bb-736fb4d8a922>
2013-08-19 04:56:57 +0000

    Automatic build of sidi-util

---
+++ tags/2.4.0.891/Sidi.Util/CommandLine/Application.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+namespace Sidi.CommandLine
+{
+    public class Application
+    {
+        public Application(object instance)
+        {
+            this.Instance = instance;
+        }
+
+        public object Instance { get; private set; }
+        public bool? ProgramSpecificPreferences { get; set; }
+
+        public override string ToString()
+        {
+            return Instance.GetType().ToString();
+        }
+
+        /// <summary>
+        /// All available actions and options
+        /// </summary>
+        public IEnumerable<IParserItem> FindItems(Parser parser)
+        {
+            foreach (MethodInfo i in Instance.GetType().GetMethods())
+            {
+                if (ValueParser.IsSuitable(i))
+                {
+                    // yield return new ValueParser(this, application, i);
+                }
+                else
+                {
+                    string u = Usage.Get(i);
+                    string parameters = String.Join(" ", Array.ConvertAll(i.GetParameters(), new Converter<ParameterInfo, string>(delegate(ParameterInfo pi)
+                    {
+                        return String.Format("[{1} {0}]", pi.Name, pi.ParameterType.GetInfo());
+                    })));
+                    if (u != null)
+                    {
+                        yield return new Action(parser, this, i);
+                    }
+                }
+            }
+
+            foreach (MemberInfo i in Instance.GetType().GetMembers())
+            {
+                if (SubCommand.IsSubCommand(i))
+                {
+                    if ((i is FieldInfo || i is PropertyInfo))
+                    {
+                        var subCommand = new SubCommand(parser, this, i);
+                        yield return subCommand;
+                    }
+                }
+                else
+                {
+                    string u = Usage.Get(i);
+                    if ((i is FieldInfo || i is PropertyInfo) && u != null)
+                    {
+                        yield return new Option(parser, this, i);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// All value parsers
+        /// </summary>
+        public IList<ValueParser> GetValueParsers(Parser parser)
+        {
+            return Instance.GetType().GetMethods()
+                .Where(i => ValueParser.IsSuitable(i))
+                .Select(i => new ValueParser(parser, this, i))
+                .ToList();
+        }
+    }
+}
---
+++ tags/2.4.0.891/Sidi.Util/CommandLine/LogOptions.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using log4net.Repository.Hierarchy;
+using log4net;
+using System.ComponentModel;
+using System.Reflection;
+using log4net.Layout;
+using log4net.Appender;
+using Sidi.IO;
+
+namespace Sidi.CommandLine
+{
+    [Usage("Adjust logging behavior")]
+    public class LogOptions
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public LogOptions(Parser parser)
+        {
+            this.parser = parser;
+        }
+
+        Parser parser;
+
+        [Usage("Log level. Determines the verbosity of logging output.")]
+        [Example("off")]
+        [Example("error")]
+        [Example("warn")]
+        [Example("info")]
+        [Example("debug")]
+        [Example("all")]
+        public static log4net.Core.Level ParseLevel(string stringRepresentation)
+        {
+            var levels = typeof(log4net.Core.Level).GetFields(BindingFlags.Static | BindingFlags.Public);
+            var selected = levels.First(i => Parser.IsMatch(stringRepresentation, i.Name));
+            return (log4net.Core.Level)selected.GetValue(null);
+        }
+
+        [Usage("Determines the verbosity of logging output (off, error, warn, info, debug, all)"), Persistent]
+        [Category(Parser.categoryLogging)]
+        public log4net.Core.Level LogLevel
+        {
+            get
+            {
+                var hierarchy = (Hierarchy)LogManager.GetRepository();
+                return hierarchy.Root.Level;
+            }
+
+            set
+            {
+                ConfigureDefaultLogging();
+                var hierarchy = (Hierarchy)LogManager.GetRepository();
+                hierarchy.Root.Level = value;
+            }
+        }
+
+        static string logFileAppenderName = "LogFile";
+        
+        [Usage("Write a log file"), Persistent]
+        [Category(Parser.categoryLogging)]
+        public bool LogFile
+        {
+            get
+            {
+                var hierarchy = (Hierarchy)LogManager.GetRepository();
+                return hierarchy.Root.GetAppender(logFileAppenderName) != null;
+            }
+
+            set
+            {
+                ConfigureDefaultLogging();
+                var hierarchy = (Hierarchy)LogManager.GetRepository();
+                var lf = hierarchy.Root.GetAppender(logFileAppenderName);
+                if (value)
+                {
+                    if (lf == null)
+                    {
+                        var exeFileName = System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName;
+                        var appType = this.parser.StartupApplication.Instance.GetType();
+                        var file = Paths.Temp.CatDir(
+                                "log",
+                                LPath.GetValidFilename(exeFileName),
+                                LPath.GetValidFilename(String.Format("{1}_{0}.txt", DateTime.Now.ToString("o"), exeFileName)));
+                        file.EnsureParentDirectoryExists();
+                        log.InfoFormat("Log file: {0}", file);                
+                        var newLf = new FileAppender()
+                        {
+                            AppendToFile = false,
+                            File = file,
+                            Layout = pattern,
+                            Name = logFileAppenderName
+                        };
+
+                        newLf.ActivateOptions();
+
+                        hierarchy.Root.AddAppender(newLf);
+                        
+                    }
+                }
+                else
+                {
+                    if (lf != null)
+                    {
+                        hierarchy.Root.RemoveAppender(lf);
+                    }
+                }
+            }
+        }
+
+        static log4net.Layout.ILayout pattern = new PatternLayout("%utcdate{ISO8601} [%thread] %level %logger %ndc - %message%newline");
+        
+        public static void ConfigureDefaultLogging()
+        {
+            var hierarchy = (Hierarchy)LogManager.GetRepository();
+            if (hierarchy.Configured)
+            {
+                return;
+            }
+
+            var ca = new ConsoleAppender()
+            {
+                Target = "Console.Error",
+                Layout = pattern,
+            };
+
+            hierarchy.Root.AddAppender(ca);
+            hierarchy.Configured = true;
+        }
+
+        [Usage("Reset log options to defaults")]
+        [Category(Parser.categoryLogging)]
+        public void LogReset()
+        {
+            LogLevel = log4net.Core.Level.Info;
+            LogFile = false;
+        }
+    }
+}
