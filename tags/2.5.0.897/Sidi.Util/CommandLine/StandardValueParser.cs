e9ed46ea7eddaefd506f6dd041f26aeccbd897a1
andreas.grimme@googlemail.com <andreas.grimme@googlemail.com@8a84ef9e-818e-11dd-b2bb-736fb4d8a922>
2013-08-25 09:11:20 +0000

    Automatic build of sidi-util

---
+++ tags/2.5.0.897/Sidi.Util/CommandLine/StandardValueParser.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using Sidi.Extensions;
+
+namespace Sidi.CommandLine
+{
+    public class StandardValueParser : IValueParser
+    {
+        public StandardValueParser(Type type)
+        {
+            this.type = type;
+
+            var parse = type.GetMethod("Parse", BindingFlags.Static | BindingFlags.Public);
+            if (parse != null)
+            {
+                goto ok;
+            }
+
+            var ctor = type.GetConstructor(new Type[] { typeof(string) });
+            if (ctor != null)
+            {
+                goto ok;
+            }
+
+            throw new InvalidCastException(type.ToString() + " is not supported");
+        ok: ;
+        }
+
+        Type type;
+
+        public string Usage
+        {
+            get { return String.Format("{0} value", type.Name); }
+        }
+
+        public string UsageText
+        {
+            get { return String.Format("{0} value", type.Name); }
+        }
+
+        public string Name
+        {
+            get { return type.Name; }
+        }
+
+        public string Syntax
+        {
+            get { return type.Name; }
+        }
+
+        public ItemSource Source
+        {
+            get { return null; }
+        }
+
+        public IEnumerable<string> Categories
+        {
+            get { return new string[] { }; }
+        }
+
+        public Uri ReferenceUri
+        {
+            get
+            {
+                return new Uri(String.Format(@"http://msdn.microsoft.com/library/{0}.aspx", type.FullName));
+            }
+        }
+
+        public object Handle(IList<string> args, bool execute)
+        {
+            var stringRepresentation = args.PopHead();
+
+            var parse = type.GetMethod("Parse", BindingFlags.Static | BindingFlags.Public);
+            if (parse != null)
+            {
+                return parse.Invoke(null, new object[] { stringRepresentation });
+            }
+
+            var ctor = type.GetConstructor(new Type[] { typeof(string) });
+            if (ctor != null)
+            {
+                return ctor.Invoke(new object[] { stringRepresentation });
+            }
+
+            throw new InvalidCastException(type.ToString() + " is not supported");
+        }
+    }
+}
---
+++ tags/2.5.0.897/Sidi.Util/CommandLine/ValueParser.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.ComponentModel;
+using System.IO;
+using Sidi.Extensions;
+using Sidi.Util;
+
+namespace Sidi.CommandLine
+{
+    public interface IValueParser : IParserItem
+    {
+    }
+
+    public class ValueParser : IValueParser
+    {
+        public ValueParser(Parser parser, ItemSource source, MethodInfo method)
+        {
+            Source = source;
+            MethodInfo = method;
+            this.parser = parser;
+        }
+
+        public static bool IsSuitable(MethodInfo m)
+        {
+            var p = m.GetParameters();
+            return m.Name.StartsWith("Parse")
+                && m.IsStatic
+                && p.Length == 1
+                && (p[0].ParameterType.Equals(typeof(string)) || p[0].ParameterType.Equals(typeof(IList<string>)));
+        }
+
+        Parser parser;
+
+        // must be like static bool Parse(string stringRepresentation, out <Type> result)
+        public MethodInfo MethodInfo { private set; get; }
+
+        public ItemSource Source { private set; get; }
+
+        public string Usage
+        {
+            get { return Sidi.CommandLine.Usage.Get(MethodInfo); }
+        }
+
+        public IEnumerable<ExampleAttribute> Examples
+        {
+            get
+            {
+                return ExampleAttribute.Get(MethodInfo);
+            }
+        }
+
+        public string UsageText
+        {
+            get
+            {
+                var s = new StringWriter();
+                s.Write(Usage);
+                var e = Examples;
+                if (e.Any())
+                {
+                    s.Write(". Examples: ");
+                    s.Write(e.Select(x => x.Value.Quote()).Join(", "));
+                }
+                return s.ToString();
+            }
+        }
+
+        public string Syntax
+        {
+            get { return ValueType.Name; }
+        }
+
+        public string Name
+        {
+            get { return ValueType.Name; }
+        }
+
+        public IEnumerable<string> Categories
+        {
+            get
+            {
+                var c = MethodInfo.GetCustomAttributes(typeof(CategoryAttribute), true)
+                    .Select(x => ((CategoryAttribute)x).Category);
+                return c.Any() ? c : new string[] { String.Empty };
+            }
+        }
+
+        public Type ValueType
+        {
+            get
+            {
+                return MethodInfo.ReturnType;
+            }
+        }
+
+        public object Handle(IList<string> args, bool execute)
+        {
+            object result = null;
+            if (this.MethodInfo.GetParameters()[0].ParameterType.Equals(typeof(IList<string>)))
+            {
+                try
+                {
+                    result = this.MethodInfo.Invoke(null, new object[] { args });
+                }
+                catch
+                {
+                    var p = Tokenizer.ToList(args[0]);
+                    result = this.MethodInfo.Invoke(null, new object[] { p });
+                    args.PopHead();
+                }
+            }
+            else
+            {
+                result = this.MethodInfo.Invoke(null, new object[] { args[0] });
+                args.PopHead();
+            }
+            return result;
+        }
+    }
+}
