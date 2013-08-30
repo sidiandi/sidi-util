dab4539fb69f2857e02055fb4315346a5f1d261e
andreas.grimme@googlemail.com <andreas.grimme@googlemail.com@8a84ef9e-818e-11dd-b2bb-736fb4d8a922>
2013-08-30 03:57:31 +0000

    Automatic build of sidi-util

---
+++ tags/2.5.0.901/Sidi.Util/Util/RegexIgnoreCase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Sidi.Util
+{
+    public class RegexIgnoreCase : Regex
+    {
+        public RegexIgnoreCase(string pattern)
+        : base(pattern, RegexOptions.IgnoreCase)
+        {
+        }
+    }
+}
