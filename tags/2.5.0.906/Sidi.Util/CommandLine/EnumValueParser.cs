8bc846eee54e9ac51e26bf50d51eff1cefa2e215
andreas.grimme@googlemail.com <andreas.grimme@googlemail.com@8a84ef9e-818e-11dd-b2bb-736fb4d8a922>
2013-09-20 01:45:48 +0000

    Automatic build of sidi-util

---
+++ tags/2.5.0.906/Sidi.Util/CommandLine/EnumValueParser.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Sidi.Extensions;
+
+namespace Sidi.CommandLine
+{
+    public class EnumValueParser : IValueParser
+    {
+        public EnumValueParser(Type type)
+        {
+            this.type = type;
+        }
+
+        Type type;
+
+        public string Usage
+        {
+            get { return String.Format("{0} value", type.Name); }
+        }
+
+        public string UsageText
+        {
+            get { return String.Format("{0} enum. One of: {1}", type.Name, Enum.GetValues(type).Cast<object>().Join(", ")); }
+        }
+
+        public string Name
+        {
+            get { return type.Name; }
+        }
+
+        public string Syntax
+        {
+            get
+            {
+                return Enum.GetValues(type).Cast<object>().Join("|");
+            }
+        }
+
+        public ItemSource Source
+        {
+            get { return null; }
+        }
+
+        public IEnumerable<string> Categories
+        {
+            get { return new string[] { }; }
+        }
+
+        public object Handle(IList<string> args, bool execute)
+        {
+            return Enum.Parse(type, args.PopHead(), true);
+        }
+    }
+}
---
+++ tags/2.5.0.906/Sidi.Util/IO/PathList.cs
@@ -0,0 +1,102 @@
+// Copyright (c) 2012, Andreas Grimme (http://andreas-grimme.gmxhome.de/)
+// 
+// This file is part of sidi-util.
+// 
+// sidi-util is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// sidi-util is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with sidi-util. If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Sidi.Util;
+using Sidi.Extensions;
+using System.Collections;
+using System.Collections.Specialized;
+
+namespace Sidi.IO
+{
+    [Serializable]
+    public class PathList : List<LPath>
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public PathList(IEnumerable<LPath> paths)
+        : base(paths)
+        {
+        }
+
+        public PathList()
+        {
+        }
+
+        public static PathList ReadClipboard()
+        {
+            var fileList = new PathList();
+            fileList.AddRange(Clipboard.GetFileDropList()
+                .Cast<string>()
+                .Select(x => new Sidi.IO.LPath(x)));
+            return fileList;
+        }
+
+        public void WriteClipboard()
+        {
+            var files = new StringCollection();
+            foreach (var i in this)
+            {
+                files.Add(i);
+            }
+            Clipboard.SetFileDropList(files);
+        }
+
+        public static PathList GetFilesSelectedInExplorer()
+        {
+            var s = new Shell();
+            return new PathList(s.SelectedFiles);
+        }
+
+        public static PathList Get(IDataObject data)
+        {
+            if (data.GetDataPresent(DataFormats.FileDrop))
+            {
+                return new PathList(((string[])data.GetData(DataFormats.FileDrop))
+                    .Select(x => new LPath(x)));
+            }
+            else
+            {
+                return new PathList();
+            }
+        }
+
+        public static PathList Parse(string files)
+        {
+            if (files.Equals(":paste", StringComparison.InvariantCultureIgnoreCase))
+            {
+                return ReadClipboard();
+            }
+
+            if (files.Equals(":sel", StringComparison.OrdinalIgnoreCase))
+            {
+                return GetFilesSelectedInExplorer();
+            }
+
+            return new PathList(files.Split(new[] { ";" }, StringSplitOptions.None).Select(x => new Sidi.IO.LPath(x.Unquote())));
+        }
+
+        public override string ToString()
+        {
+            return this.Join(";");
+        }
+    }
+}
