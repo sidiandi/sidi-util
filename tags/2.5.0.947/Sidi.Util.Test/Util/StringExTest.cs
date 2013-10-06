9d774169dc35f403154003b23c020a792255e925
andreas.grimme@googlemail.com <andreas.grimme@googlemail.com@8a84ef9e-818e-11dd-b2bb-736fb4d8a922>
2013-10-06 05:29:47 +0000

    Automatic build of sidi-util

---
+++ tags/2.5.0.947/Sidi.Util.Test/Util/StringExTest.cs
@@ -0,0 +1,60 @@
+// Copyright (c) 2009, Andreas Grimme (http://andreas-grimme.gmxhome.de/)
+// 
+// This file is part of sidi-util.
+// 
+// sidi-util is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// sidi-util is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with sidi-util. If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using System.IO;
+using Sidi.Util;
+using Sidi.Extensions;
+using Sidi.Test;
+
+namespace Sidi.Util
+{
+    [TestFixture]
+    public class StringExTest : TestBase
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        
+        [Test]
+        public void GetSection()
+        {
+            StringWriter w = new StringWriter();
+            w.WriteLine("[SectionA]");
+            w.WriteLine("hello");
+            w.WriteLine("[SectionB]");
+            w.WriteLine("world");
+
+            string t = w.ToString();
+
+            Assert.AreEqual("hello", t.GetSection("SectionA"));
+            Assert.AreEqual("world", t.GetSection("SectionB"));
+        }
+
+        [Test]
+        public void SafeToString()
+        {
+            object o = null;
+            Assert.AreEqual(String.Empty, o.SafeToString());
+
+            o = "Hello";
+            Assert.AreEqual("Hello", o.SafeToString());
+        }
+    }
+}
---
+++ tags/2.5.0.947/Sidi.Util/Extensions/IListExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sidi.Extensions
+{
+    public static class IListExtensions
+    {
+        public static T Pop<T>(this IList<T> list)
+        {
+            var last = list.Count-1;
+            var r = list[last];
+            list.RemoveAt(last);
+            return r;
+        }
+    }
+}
