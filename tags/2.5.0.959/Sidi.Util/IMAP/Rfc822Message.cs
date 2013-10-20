5124b4c07764db0b3761f273d1db91d1c37bbf00
andreas.grimme@googlemail.com <andreas.grimme@googlemail.com@8a84ef9e-818e-11dd-b2bb-736fb4d8a922>
2013-10-20 03:53:53 +0000

    Automatic build of sidi-util

---
+++ tags/2.5.0.959/Sidi.Util/IMAP/Rfc822Message.cs
@@ -0,0 +1,212 @@
+// Copyright (c) 2012, Andreas Grimme (http://andreas-grimme.gmxhome.de/)
+// 
+// This file is part of sidi-util.
+// 
+// sidi-util is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// sidi-util is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with sidi-util. If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Reflection;
+using Sidi.Util;
+using Sidi.Extensions;
+
+namespace Sidi.IMAP
+{
+    public class Rfc822DateTime
+    {
+        public DateTime Date;
+        public TimeSpan TimeZone;
+
+        static readonly string[] months = new string[] { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
+
+        public override string ToString()
+        {
+                var t = Date;
+                return String.Format(
+                    "{0:D2}-{1}-{2:D4} {3:D2}:{4:D2}:{5:D2} {6}{7:D2}{8:D2}",
+                    t.Day,
+                    Rfc822DateTime.months[t.Month-1],
+                    t.Year,
+                    t.Hour,
+                    t.Minute,
+                    t.Second,
+                    TimeZone.TotalSeconds < 0 ? "-" : "+",
+                    Math.Abs(TimeZone.Hours),
+                    Math.Abs(TimeZone.Minutes));
+        }
+        
+        public static Rfc822DateTime Parse(string date)
+        {
+            var p = new ArgParser(date);
+
+            var dayRegex = new Regex("Mon|Tue|Wed|Thu|Fri|Sat|Sun");
+            var monthRegex = new Regex(months.Join("|"));
+
+            if (p.IsNext(dayRegex))
+            {
+                var weekday = p.Get(dayRegex);
+                p.Consume(",");
+            }
+            p.SkipSpace();
+            var day = p.GetNumber();
+            p.SkipSpace();
+            var monthName = p.Get(monthRegex);
+            var month = 1 + Array.IndexOf(months, monthName);
+            p.SkipSpace();
+            var year = p.GetNumber();
+            p.SkipSpace();
+
+            var hour = p.GetNumber();
+            p.Consume(":");
+            var minute = p.GetNumber();
+            var second = 0;
+            if (p.ConsumeIfNext(":"))
+            {
+                second = p.GetNumber();
+            }
+
+            var zoneName = new string[]{"UT", "GMT", "EST", "EDT", "CST", "CDT", "MST", "MDT", "PST", "PDT", "1ALPHA"};
+            var zoneOffset = new int[]{ 0   , 0    , -5,    -4,    -6,    -5,    -7,    -6,    -8,    -7,    -1};
+            var zoneRegex = new Regex(zoneName.Join("|"));
+
+
+            int timeZone = 0; // in seconds
+            p.SkipSpace();
+            if (p.IsNext(zoneRegex))
+            {
+                var zone = p.Get(zoneRegex);
+                timeZone = zoneOffset[Array.IndexOf(zoneName, zone)] * 3600;
+            }
+            else
+            {
+                int sign = 0;
+                if (p.ConsumeIfNext("-"))
+                {
+                    sign = -1;
+                }
+                else
+                {
+                    p.Consume("+");
+                    sign = 1;
+                }
+
+                timeZone = sign * (60 * p.GetNumber(2) + p.GetNumber(2)) * 60;
+            }
+
+            return new Rfc822DateTime()
+            {
+                Date = new DateTime(year, month, day, hour, minute, second),
+                TimeZone = TimeSpan.FromSeconds(timeZone)
+            };
+        }
+    }
+
+    public class Rfc822Message
+    {
+        public Rfc822Message()
+        {
+        }
+
+        string GetHeaderValue(string key)
+        {
+            return Headers
+                .First(kvp => kvp.Key.Equals(key, StringComparison.InvariantCultureIgnoreCase))
+                .Value;
+        }
+
+        string GetHeaderValue(MethodBase m)
+        {
+            return GetHeaderValue(m.Name.Substring("get_".Length).Replace("_", "-"));
+        }
+
+        public string Header
+        {
+            get
+            {
+                return FormatHeaders(Headers);
+            }
+        }
+
+        public string HeaderFields(string[] fields)
+        {
+            return FormatHeaders(Headers.Where(x => fields.Any(f => f.Equals(x.Key, StringComparison.InvariantCultureIgnoreCase))));
+        }
+
+        public string HeaderFieldsNot(string[] fields)
+        {
+            return FormatHeaders(Headers.Where(x => !fields.Any(f => f.Equals(x.Key, StringComparison.InvariantCultureIgnoreCase))));
+        }
+
+        const string headerEnd = "\r\n";
+
+        static string ToString(KeyValuePair<string, string> headerItem)
+        {
+            return String.Format("{0}: {1}", headerItem.Key, headerItem.Value);
+        }
+
+        static string FormatHeaders(IEnumerable<KeyValuePair<string, string>> headers)
+        {
+            return headers.Select(x => ToString(x)).Join() + headerEnd;
+        }
+
+        public string Subject { get { return GetHeaderValue(MethodBase.GetCurrentMethod()); } }
+        public Rfc822DateTime Date { get { return Rfc822DateTime.Parse(GetHeaderValue(MethodBase.GetCurrentMethod())); } }
+        public string From { get { return GetHeaderValue(MethodBase.GetCurrentMethod()); } }
+        public string To { get { return GetHeaderValue(MethodBase.GetCurrentMethod()); } }
+        public string Message_ID { get { return GetHeaderValue(MethodBase.GetCurrentMethod()); } }
+
+        public string Content_Type { get { return GetHeaderValue(MethodBase.GetCurrentMethod()); } }
+        public string Content_Transfer_Encoding { get { return GetHeaderValue(MethodBase.GetCurrentMethod()); } }
+        public int Size { get { return Text.Length; } }
+
+        public static Rfc822Message Parse(TextReader r)
+        {
+            var p = new ArgParser(String.Empty, r, null);
+            var m = new Rfc822Message() { Headers = new List<KeyValuePair<string, string>>() };
+            p.NextLine();
+            while (!String.IsNullOrEmpty(p.Rest))
+            {
+                m.Headers.Add(Field(p));
+            }
+            m.Text = p.input.ReadToEnd();
+            return m;
+        }
+
+        static KeyValuePair<string, string> Field(ArgParser p)
+        {
+            var key = p.Get(new Regex(@"[\w-]+"));
+            p.Get(new Regex(@"\s*"));
+            p.Consume(@":");
+            using (var value = new StringWriter())
+            {
+                value.Write(p.Rest);
+                p.NextLine();
+                while (p.IsNext(new Regex(@"\s+")))
+                {
+                    value.WriteLine();
+                    value.Write(p.Rest);
+                    p.NextLine();
+                }
+                return new KeyValuePair<string, string>(key, value.ToString().Trim());
+            }
+        }
+
+        public List<KeyValuePair<string, string>> Headers { get; set; }
+        public string Text { get; set; }
+    }
+}
---
+++ tags/2.5.0.959/Sidi.Util/Util/GraMath.cs
@@ -0,0 +1,286 @@
+// Copyright (c) 2012, Andreas Grimme (http://andreas-grimme.gmxhome.de/)
+// 
+// This file is part of sidi-util.
+// 
+// sidi-util is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// sidi-util is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with sidi-util. If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing.Drawing2D;
+using System.Drawing;
+
+namespace Sidi.Util
+{
+    public class GraMath
+    {
+        static public Rectangle SizeToFit(Rectangle s, Rectangle d, double os)
+        {
+            if (s.Width == 0 || s.Height == 0)
+            {
+                return new Rectangle();
+            }
+
+            double o;
+            if (s.Width * d.Height > d.Width * s.Height)
+            {
+                o = 0.0;
+            }
+            else
+            {
+                o = 1.0;
+            }
+
+            double scale =
+                o * ((double)(d.Width) / (double)(s.Width)) +
+                (1 - o) * ((double)(d.Height) / (double)(s.Height));
+
+            if (os < 1.0)
+            {
+                scale = Math.Min(scale, ((double)d.Width / (double)s.Width) / (1 - os));
+                scale = Math.Min(scale, ((double)d.Height / (double)s.Height) / (1 - os));
+            }
+
+            Rectangle i = new Rectangle(
+                0, 0,
+                (int)(scale * (double)s.Width),
+                (int)(scale * (double)s.Height)
+                );
+
+            return Center(i, d);
+        }
+
+        /// <summary>
+        /// Calculates the 2D transform to required convert rectangle src to rectangle dst
+        /// </summary>
+        /// <param name="src"></param>
+        /// <param name="dst"></param>
+        /// <returns></returns>
+        public static Matrix CalcTransform(Rectangle src, Rectangle dst)
+        {
+            Point[] p = {
+							new Point(dst.Left, dst.Top), 
+							new Point(dst.Right, dst.Top), 
+							new Point(dst.Left, dst.Bottom)
+						};
+            return new Matrix(src, p);
+        }
+
+        /// <summary>
+        /// Returns the center point of the rectangle.
+        /// </summary>
+        /// <param name="r"></param>
+        /// <returns></returns>
+        static Point Center(Rectangle r)
+        {
+            return new Point(r.Left + r.Width / 2, r.Top + r.Height / 2);
+        }
+
+        /// <summary>
+        /// returns the area for a size.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static double Area(Size s)
+        {
+            return (double)s.Width * (double)s.Height;
+        }
+
+        public static void Tile(Size a, int minCount, out Size n, out SizeF itemSize)
+        {
+            float ix = (float) Math.Sqrt(Area(a) / (double)Math.Max(1, minCount));
+
+            n = new Size
+            (
+                Math.Max(1, (int)((float)a.Width / ix)),
+                Math.Max(1, (int)((float)a.Height / ix))
+            );
+
+            for (;;)
+            {
+                itemSize = new SizeF((float)a.Width / (float)n.Width, (float)a.Height / (float)n.Height);
+                if (n.Width * n.Height >= minCount)
+                {
+                    break;
+                }
+                if (itemSize.Width > itemSize.Height)
+                {
+                    ++n.Width;
+                }
+                else
+                {
+                    ++n.Height;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Centers rectangle r in rectangle and returns it.
+        /// </summary>
+        /// <param name="r"></param>
+        /// <param name="at"></param>
+        /// <returns></returns>
+        static public Rectangle Center(Rectangle r, Rectangle at)
+        {
+            return new Rectangle(
+                at.Left + at.Width / 2 - r.Width / 2,
+                at.Top + at.Height / 2 - r.Height / 2,
+                r.Width, r.Height);
+        }
+
+        static public Point Mul(Point a, int b)
+        {
+            return new Point(a.X * b, a.Y * b);
+        }
+
+        static public RectangleF Transform(RectangleF r, Matrix m)
+        {
+            PointF[] p = { r.Location, r.Location + r.Size };
+            m.TransformPoints(p);
+            return new RectangleF(
+                p[0].X, p[0].Y,
+                p[1].X - p[0].X, p[1].Y - p[0].Y);
+        }
+
+        static public RectangleF TransformBoundingBox(RectangleF r, Matrix m)
+        {
+            PointF[] p = { 
+                new PointF(r.Left, r.Top),
+                new PointF(r.Right, r.Top),
+                new PointF(r.Right, r.Bottom),
+                new PointF(r.Left, r.Bottom),
+            };
+
+            m.TransformPoints(p);
+            return BoundingBox(p);
+        }
+
+        static public RectangleF BoundingBox(PointF[] points)
+        {
+            int iEnd = points.Length;
+
+            if (iEnd == 0)
+            {
+                return new RectangleF();
+            }
+
+            RectangleF b = new RectangleF(points[0], new SizeF(0, 0));
+
+            for (int i=1; i < iEnd; ++i)
+            {
+                PointF p = points[i];
+                if (p.X > b.Right)
+                {
+                    b.Width = p.X - b.Left;
+                }
+                if (p.X < b.Left)
+                {
+                    b.X = p.X;
+                }
+                if (p.Y > b.Bottom)
+                {
+                    b.Height = p.Y - b.Top;
+                }
+                if (p.Y < b.Top)
+                {
+                    b.Y = p.Y;
+                }
+            }
+
+            return b;
+
+        }
+
+        static public int Floor(int x, int step)
+        {
+            return (x / step) * step;
+        }
+
+        static public int Ceil(int x, int step)
+        {
+            int f = Floor(x, step);
+            if (f < x)
+            {
+                f += step;
+            }
+            return f;
+        }
+
+        public static int Clip(int minimum, int x, int maximum)
+        {
+            if (x < minimum)
+            {
+                return minimum;
+            }
+            if (x < maximum)
+            {
+                return x;
+            }
+
+            return maximum - 1;
+        }
+
+        public static float Clip(float minimum, float x, float maximum)
+        {
+            if (x < minimum)
+            {
+                return minimum;
+            }
+            if (x < maximum)
+            {
+                return x;
+            }
+
+            return maximum;
+        }
+
+        static public bool Contains(Rectangle c, Rectangle r)
+        {
+            return
+                c.Left <= r.Left && r.Right <= c.Right &&
+                c.Top <= r.Top && r.Bottom <= c.Bottom;
+        }
+
+        public static int CeilingPower2(int x)
+        {
+            int y = 1;
+            for (; y < x; y *= 2)
+            {
+            }
+            return y;
+        }
+
+        public static Rectangle BoundsRect(int left, int right, int top, int bottom)
+        {
+            return new Rectangle(left, top, right - left, bottom - top);
+        }
+        
+        public static int WrapAround(int x, int modulo)
+        {
+            if (x < 0)
+            {
+                int r = ((modulo-x) % modulo);
+                if (r == 0)
+                {
+                    r = modulo;
+                }
+                return modulo - r;
+            }
+            else
+            {
+                return x % modulo;
+            }
+        }
+    }
+}
