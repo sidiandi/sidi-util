088edfec0df38c8984b0497c41b23feefe3014d7
andreas.grimme@googlemail.com <andreas.grimme@googlemail.com@8a84ef9e-818e-11dd-b2bb-736fb4d8a922>
2013-10-24 17:10:22 +0000

    Automatic build of sidi-util

---
+++ tags/2.5.0.961/IMAP-test/Properties/AssemblyInfo.cs
@@ -0,0 +1,53 @@
+// Copyright (c) 2009, Andreas Grimme (http://andreas-grimme.gmxhome.de/)
+// 
+// This file is part of sidi-util.
+// 
+// sidi-util is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// sidi-util is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with sidi-util. If not, see <http://www.gnu.org/licenses/>.
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("IMAP-test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("sidi")]
+[assembly: AssemblyProduct("IMAP-test")]
+[assembly: AssemblyCopyright("Copyright © Andreas Grimme")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2bf07eca-345c-4108-8370-651a1f6f2015")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
