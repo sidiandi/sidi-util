e16d17d14b66c1118ca991a6ae3be61f0cbd15c3
andreas.grimme@googlemail.com <andreas.grimme@googlemail.com@8a84ef9e-818e-11dd-b2bb-736fb4d8a922>
2013-10-26 11:49:51 +0000

    Automatic build of sidi-util

---
+++ tags/2.5.0.967/Sidi.Util/CommandLine/ValueParserAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sidi.CommandLine
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple=true)]
+    public class ValueParserAttribute : System.Attribute 
+    {
+        public ValueParserAttribute(Type type)
+        {
+            ValueParserType = type;
+        }
+
+        public Type ValueParserType { get; private set; }
+    }
+}
