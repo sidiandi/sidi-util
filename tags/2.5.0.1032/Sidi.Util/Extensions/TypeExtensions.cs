ed699ee23a112a2d182dd1d20b72bbb7758e6572
andreas.grimme@googlemail.com <andreas.grimme@googlemail.com@8a84ef9e-818e-11dd-b2bb-736fb4d8a922>
2014-02-22 19:27:54 +0000

    Automatic build of sidi-util

---
+++ tags/2.5.0.1032/Sidi.Util/Extensions/TypeExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Sidi.IO;
+using System.Reflection;
+
+namespace Sidi.Extensions
+{
+    public static class TypeExtensions
+    {
+        public static T GetAssemblyAttribute<T>(this Type t)
+        {
+            var a = t.Assembly;
+            try
+            {
+                return ((T)a.GetCustomAttributes(typeof(T), false).First());
+            }
+            catch (Exception)
+            {
+                throw new Exception(String.Format("{0} is not defined for assembly {1}", typeof(T).ToString(), a));
+            }
+        }
+    }
+}
