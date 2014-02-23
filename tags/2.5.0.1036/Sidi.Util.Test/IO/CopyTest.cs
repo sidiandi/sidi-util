18f551694da3ed29faa37eff6d8cb8bf8a842b03
andreas.grimme@googlemail.com <andreas.grimme@googlemail.com@8a84ef9e-818e-11dd-b2bb-736fb4d8a922>
2014-02-23 09:28:34 +0000

    Automatic build of sidi-util

---
+++ tags/2.5.0.1036/Sidi.Util.Test/IO/CopyTest.cs
@@ -0,0 +1,40 @@
+// Copyright (c) 2009, Andreas Grimme (http://andreas-grimme.gmxhome.de/)
+// 
+// This file is part of sidi-util.
+// 
+// sidi-util is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// sidi-util is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with sidi-util. If not, see <http://www.gnu.org/licenses/>.
+
+#region "Mandatory NUnit Imports"
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+using NUnit.Framework;
+using Sidi.IO;
+using System.IO;
+using Sidi.Test;
+#endregion
+
+namespace Sidi.Util
+{
+    [TestFixture]
+    public class CopyTest : TestBase
+    {
+        [Test]
+        public void Copy()
+        {
+            // todo
+        }
+    }
+}
