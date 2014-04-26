8df432f38444d72b84eff0fa5bd1326e391c2cb2
andreas.grimme@googlemail.com <andreas.grimme@googlemail.com@8a84ef9e-818e-11dd-b2bb-736fb4d8a922>
2014-04-26 07:25:59 +0000

    Automatic build of sidi-util

---
+++ tags/2.5.0.1064/Sidi.Util/CommandLine/Parameter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sidi.CommandLine
+{
+    public class Parameter
+    {
+        public IValueParser ValueParser;
+        public System.Reflection.ParameterInfo ParameterInfo;
+    }
+}
