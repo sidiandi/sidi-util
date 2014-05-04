833894489bcc667f786907261b10015d54689d75
andreas.grimme@googlemail.com <andreas.grimme@googlemail.com@8a84ef9e-818e-11dd-b2bb-736fb4d8a922>
2014-05-04 06:43:56 +0000

    Automatic build of sidi-util

---
+++ tags/2.5.0.1093/Sidi.Util/Extensions/AssemblyExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+namespace Sidi.Extensions
+{
+    public static class AssemblyExtensions
+    {
+        public static T GetCustomAttribute<T>(this Assembly a)
+        {
+            return (T) a.GetCustomAttributes(typeof(T), false).First();
+        }
+    }
+}
