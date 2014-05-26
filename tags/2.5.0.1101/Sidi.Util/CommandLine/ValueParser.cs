c7172c239c60c0764562f024f914ccfa912998ac
andreas.grimme@googlemail.com <andreas.grimme@googlemail.com@8a84ef9e-818e-11dd-b2bb-736fb4d8a922>
2014-05-26 08:36:40 +0000

    Automatic build of sidi-util

---
+++ tags/2.5.0.1101/Sidi.Util/CommandLine/ValueParser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.ComponentModel;
+using System.IO;
+using Sidi.Extensions;
+using Sidi.Util;
+
+namespace Sidi.CommandLine
+{
+    public interface IValueParser : IParserItem
+    {
+        Type ValueType { get; }
+        IEnumerable<ExampleAttribute> Examples { get; }
+    }
+}
