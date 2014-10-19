e01e6895a847b80621c71403a90309423b6d6b4f
andreas.grimme@googlemail.com <andreas.grimme@googlemail.com@8a84ef9e-818e-11dd-b2bb-736fb4d8a922>
2014-10-19 08:28:01 +0000

    Automatic build of sidi-util

---
+++ tags/2.6.0.33/Sidi.Util.Test/Office/ListFormatOfficeExtensionTest.cs
@@ -0,0 +1,33 @@
+﻿using Sidi.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Sidi.Extensions;
+using Microsoft.Office.Interop.Excel;
+using NUnit.Framework;
+
+namespace Sidi.Office.Test
+{
+    [TestFixture]
+    class ListFormatOfficeExtensionTest : Sidi.Test.TestBase
+    {
+        [Test, Explicit("Opens Excel sheet")]
+        public void WriteList()
+        {
+            var sampleData = Paths.Temp.Info.GetChildren();
+
+            using (var p = ExcelProvider.GetActiveOrNew())
+            {
+                p.Application.Visible = true;
+                var sheet = p.Application.ProvideActiveWorkbook().CreateSheet();
+                sampleData
+                    .ListFormat().AllPublic()
+                    .Write(sheet.Cells);
+
+                p.KeepAlive();
+            }
+        }
+
+    }
+}
