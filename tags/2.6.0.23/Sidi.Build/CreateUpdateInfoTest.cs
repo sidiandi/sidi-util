ca752d9325e5eb7872632f8caec7376ccdd217b8
andreas.grimme@googlemail.com <andreas.grimme@googlemail.com@8a84ef9e-818e-11dd-b2bb-736fb4d8a922>
2014-10-19 08:21:13 +0000

    Automatic build of sidi-util

---
+++ tags/2.6.0.23/Sidi.Build/CreateUpdateInfoTest.cs
@@ -0,0 +1,59 @@
+// Copyright (c) 2009, Andreas Grimme (http://andreas-grimme.gmxhome.de/)
+// 
+// This file is part of sidi-util.
+// 
+// sidi-util is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// sidi-util is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with sidi-util. If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Reflection;
+using NUnit.Framework;
+using Sidi.IO;
+using Sidi.Test;
+using Sidi.Util;
+
+namespace Sidi.Build
+{
+    [TestFixture]
+    public class CreateUpdateInfoTest : TestBase
+    {
+        private static readonly log4net.ILog Log = log4net.LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        CreateUpdateInfo _instance = null;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _instance = new CreateUpdateInfo();
+        }
+
+        [Test]
+        public void Writer()
+        {
+            var outFile = TestFile(new LPath("generated.xml"));
+            {
+                var cui = new CreateUpdateInfo();
+                cui.OutputFile = outFile;
+                cui.AssemblyFile = Assembly.GetExecutingAssembly().Location;
+                cui.Execute();
+            }
+
+            var c = new UpdateCheck(Assembly.GetExecutingAssembly(), new Uri(outFile));
+            c.Check();
+            Assert.IsTrue(!c.IsUpdateRequired);
+        }
+
+    }
+}
+
+
