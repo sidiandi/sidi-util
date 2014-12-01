33b1d8319c8b34bf6de95deee61f4fdac0b9af42
andreas.grimme@googlemail.com <andreas.grimme@googlemail.com@8a84ef9e-818e-11dd-b2bb-736fb4d8a922>
2014-12-01 04:00:22 +0000

    Automatic build of sidi-util

---
+++ tags/2.6.0.73/Sidi.Util/CommandLine/BasicValueParsers.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Globalization;
+using Sidi.IO;
+using Sidi.Util;
+using Sidi.Extensions;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace Sidi.CommandLine
+{
+    [Usage("Value parsers for basic types")]
+    [ValueParser(typeof(DateTimeParser))]
+    [ValueParser(typeof(LPathParser))]
+    [ValueParser(typeof(PathListParser))]
+    [ValueParser(typeof(TimeIntervalParser))]
+    [ValueParser(typeof(TimeSpanParser))]
+    public class BasicValueParsers
+    {
+        static CultureInfo cultureInfo = (CultureInfo)CultureInfo.InvariantCulture.Clone();
+
+        [Usage("Boolean value")]
+        [Example("true")]
+        [Example("false")]
+        public static bool ParseBool(string stringRepresentation)
+        {
+            return bool.Parse(stringRepresentation);
+        }
+
+        [Usage("Integer value")]
+        [Example("42")]
+        public static int ParseInt(string stringRepresentation)
+        {
+            return int.Parse(stringRepresentation, cultureInfo);
+        }
+
+        [Usage("Floating point value")]
+        [Example("123.456")]
+        [Example("12e5")]
+        public static double ParseDouble(string stringRepresentation)
+        {
+            return double.Parse(stringRepresentation, cultureInfo);
+        }
+
+        [Usage("String")]
+        [Example("Hello")]
+        public static string ParseString(string stringRepresentation)
+        {
+            return stringRepresentation;
+        }
+    }
+}
