248a0c010f426f2d6479b6cb54780e4cfab2372f
andreas.grimme@googlemail.com <andreas.grimme@googlemail.com@8a84ef9e-818e-11dd-b2bb-736fb4d8a922>
2014-12-06 04:58:42 +0000

    Automatic build of sidi-util

---
+++ tags/3.0.0.31/Sidi.Util.Test/Visualization/TreeMapTestData.cs
@@ -0,0 +1,85 @@
+// Copyright (c) 2009, Andreas Grimme (http://andreas-grimme.gmxhome.de/)
+// 
+// This file is part of sidi-util.
+// 
+// sidi-util is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// sidi-util is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with sidi-util. If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Sidi.Test;
+
+namespace Sidi.Visualization
+{
+    [TestFixture]
+    public class TreeMapTestData : TestBase
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public static Tree GetTestTree()
+        {
+            var t = GetTestTree(null, 10, 7);
+            t.UpdateSize();
+            return t;
+        }
+
+        static Random rnd = new Random();
+
+        public static Tree GetSimpleTestTree()
+        {
+            var t = new Tree(null);
+            var count = 3;
+            foreach (var i in Enumerable.Range(1, count))
+            {
+                var c = new Tree(t);
+                c.Size = i;
+                c.Object = i;
+            }
+
+            if (t.Children.Any())
+            {
+                t.Size = 1.0f;
+            }
+            else
+            {
+                t.Size = t.ChildSize;
+            }
+            
+            return t;
+        }
+
+        static Tree GetTestTree(Tree parent, int childCount, int levels)
+        {
+            var t = new Tree(parent);
+            if (levels > 0)
+            {
+                foreach (var c in Enumerable.Range(0, rnd.Next(0, childCount)))
+                // foreach (var c in Enumerable.Range(0, childCount))
+                {
+                    var ct = GetTestTree(t, childCount, levels - 1);
+                    ct.Object = c;
+                }
+                t.Size = t.Children.Aggregate(0.0, (s, x) => s + x.Size);
+            }
+            else
+            {
+                t.Size = rnd.Next(10, 100);
+            }
+
+            return t;
+        }
+    }
+}
---
+++ tags/3.0.0.31/Sidi.Util/IO/CopyOp.cs
@@ -0,0 +1,143 @@
+// Copyright (c) 2012, Andreas Grimme (http://andreas-grimme.gmxhome.de/)
+// 
+// This file is part of sidi-util.
+// 
+// sidi-util is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// sidi-util is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with sidi-util. If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sidi.IO
+{
+    public class CopyOp
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        protected IFileSystem fs;
+
+        public void Move(LPath from, LPath to)
+        {
+            if (from.IsFile)
+            {
+                to.EnsureParentDirectoryExists();
+                fs.Move(from, to);
+            }
+            else if (from.IsDirectory)
+            {
+                to.EnsureParentDirectoryExists();
+                fs.Move(from, to);
+            }
+            log.InfoFormat("moved {0} to {1}", from, to);
+        }
+        
+        public CopyOp(IFileSystem fs = null)
+        {
+            this.fs = fs.OrDefault();
+
+            CopyRequired = (s, d) => true;
+            DoCopy = (s, d) =>
+                {
+                    d.EnsureParentDirectoryExists();
+                    fs.CopyFile(s, d);
+                };
+        }
+
+        public void Copy(LPath source, LPath destination)
+        {
+            CopyRooted(source, source, destination);
+        }
+
+        public void CopyRooted(LPath source, LPath sourceRoot, LPath destinationRoot)
+        {
+            var e = new Find()
+            {
+                Root = source,
+                Output = Find.OnlyFiles,
+            };
+
+            foreach (var s in e.Depth())
+            {
+                var d = destinationRoot.CatDir(s.FullName.RelativeTo(sourceRoot));
+                if (CopyRequired(s, d.Info))
+                {
+                    DoCopy(s.FullName, d);
+                }
+            }
+        }
+
+        public Func<IFileSystemInfo, IFileSystemInfo, bool> CopyRequired;
+        public Action<LPath, LPath> DoCopy;
+    }
+
+    public class SimpleCopyOp : CopyOp
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public bool Simulate { get; set; }
+        public bool Overwrite { get; set; }
+        public bool UseHardlinks { get; set; }
+        public enum SkipMode
+        {
+            TargetExists,
+            LastWriteTimeAndLengthEqual,
+            Never
+        }
+        public SkipMode Skip { get; set; }
+        public bool ContinueOnError { get; set; }
+
+        public SimpleCopyOp()
+        {
+            DoCopy = (s, d) =>
+                {
+                    log.InfoFormat("{0} -> {1}", s, d);
+                    if (!Simulate)
+                    {
+                        d.EnsureParentDirectoryExists();
+                        if (UseHardlinks)
+                        {
+                            try
+                            {
+                                fs.CreateHardLink(d, s);
+                            }
+                            catch (System.IO.IOException)
+                            {
+                                fs.CopyFile(s, d);
+                            }
+                        }
+                        else
+                        {
+                            fs.CopyFile(s, d);
+                        }
+                    }
+                };
+
+            this.CopyRequired = (s, d) =>
+                {
+                    switch (Skip)
+                    {
+                        case SkipMode.Never:
+                            return true;
+                        case SkipMode.LastWriteTimeAndLengthEqual:
+                            return !d.Exists || !(s.LastWriteTimeUtc.Equals(d.LastWriteTimeUtc) || s.Length.Equals(d.Length));
+                        case SkipMode.TargetExists:
+                            return !d.Exists;
+                        default:
+                            throw new System.InvalidOperationException();
+                    }
+                };
+        }
+    }
+}
