2bf2770129ebcc10f9e7df4cc665c32410f60862
andreas.grimme@googlemail.com <andreas.grimme@googlemail.com@8a84ef9e-818e-11dd-b2bb-736fb4d8a922>
2014-12-10 02:15:28 +0000

    Automatic build of sidi-util

---
+++ tags/3.0.0.44/Sidi.Util/IO/LPathExtensions.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Sidi.IO
+{
+    public static class LPathExtensions
+    {
+        public static void WriteAllText(this LPath path, string text)
+        {
+            path.EnsureParentDirectoryExists();
+            using (var w = path.WriteText())
+            {
+                w.Write(text);
+            }
+        }
+
+        public static string ReadAllText(this LPath path)
+        {
+            using (var r = path.ReadText())
+            {
+                return r.ReadToEnd();
+            }
+        }
+
+        public static void Move(this LPath source, LPath destination)
+        {
+            source.FileSystem.Move(source, destination);
+        }
+
+        public static void RemoveDirectory(this LPath path)
+        {
+            path.FileSystem.RemoveDirectory(path);
+        }
+
+        public static void DeleteFile(this LPath path)
+        {
+            path.FileSystem.DeleteFile(path);
+        }
+
+        public static void CopyFile(
+            this LPath source, 
+            LPath dest, 
+            IProgress<CopyFileProgress> progress = null,
+            System.Threading.CancellationToken cancellationToken = new CancellationToken(), 
+            CopyFileOptions options = null)
+        {
+            source.FileSystem.CopyFile(source, dest, progress, cancellationToken, options);
+        }
+
+        public static void CreateHardLink(this LPath existingFilename, LPath newHardlinkFilename)
+        {
+            existingFilename.FileSystem.CreateHardLink(newHardlinkFilename, existingFilename);
+        }
+
+        public static void CopyOrHardLink(this LPath source, LPath destination)
+        {
+            destination.EnsureParentDirectoryExists();
+            if (LPath.IsSameFileSystem(source, destination))
+            {
+                source.CreateHardLink(destination);
+            }
+            else
+            {
+                source.CopyFile(destination);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="localPath"></param>
+        public static LPath GetAdministrativeShareUnc(this LPath localPath)
+        {
+            return LPath.GetUncRoot(System.Environment.MachineName, localPath.DriveLetter + "$").CatDir(localPath.Parts);
+        }
+    }
+}
---
+++ tags/3.0.0.44/Sidi.Util/Util/ShellAccess.cs
@@ -0,0 +1,115 @@
+// Copyright (c) 2012, Andreas Grimme (http://andreas-grimme.gmxhome.de/)
+// 
+// This file is part of sidi-util.
+// 
+// sidi-util is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// sidi-util is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with sidi-util. If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Sidi.IO;
+using System.Collections;
+using SHDocVw;
+using Sidi.Extensions;
+
+namespace Sidi.Util
+{
+    public static class InternetExplorerExtension
+    {
+        public static IEnumerable<LPath> GetSelectedFiles(this SHDocVw.InternetExplorer w)
+        {
+            if (w == null)
+            {
+                return Enumerable.Empty<LPath>();
+            }
+
+            var view = ((Shell32.IShellFolderViewDual2)w.Document);
+
+            var items = view.SelectedItems()
+                .OfType<Shell32.FolderItem>()
+                .Select(i => new LPath(i.Path))
+                .ToList();
+
+            if (items.Any())
+            {
+                return items;
+            }
+
+            Console.WriteLine(w.LocationURL);
+            return new LPath[] { LPath.Parse(w.LocationURL) };
+        }
+    }
+
+    public class Shell
+    {
+        Shell32.Shell shell = new Shell32.Shell();
+
+        IEnumerable<IntPtr> GetZOrder(IntPtr parentWindow)
+        {
+            for (
+                IntPtr w = Sidi.IO.Windows.NativeMethods.GetTopWindow(parentWindow); 
+                w != IntPtr.Zero;
+                w = Sidi.IO.Windows.NativeMethods.GetWindow(w, (uint)Sidi.IO.Windows.NativeMethods.GetWindow_Cmd.GW_HWNDNEXT))
+            {
+                yield return w;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns>Topmost Windows Explorer or Internet Explorer window or null if no such window is open</returns>
+        public SHDocVw.InternetExplorer GetForegroundWindow()
+        {
+            var shellWindows = ((IEnumerable)shell.Windows()).OfType<InternetExplorer>()
+                .Select(x => { try { return new { Handle = x.HWND, Instance = x }; } catch { return null; } } )
+                .Where(x => x != null)
+                .ToDictionary(x => x.Handle, x => x.Instance);
+
+            return GetZOrder(IntPtr.Zero)
+                .Select(hwnd =>
+                    {
+                        InternetExplorer x = null;
+                        shellWindows.TryGetValue((int)hwnd, out x);
+                        return x;
+                    })
+                .Where(x => x != null)
+                .FirstOrDefault();
+        }
+
+        /// <summary>
+        /// Get selected files from topmost explorer window orr empty list if no such window exists
+        /// </summary>
+        public IEnumerable<LPath> SelectedFiles
+        {
+            get
+            {
+                return GetForegroundWindow().GetSelectedFiles();
+            }
+        }
+
+        public LPath GetOpenDirectory()
+        {
+            var w = GetForegroundWindow();
+            var url = w.LocationURL;
+            if (!Uri.IsWellFormedUriString(url, UriKind.Absolute))
+            {
+                throw new InvalidOperationException("No directory open in Windows shell");
+            }
+
+            return new LPath(new Uri(url).LocalPath);
+        }
+    }
+}
