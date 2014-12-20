bcefe66e8a07f124f88f0dec8f7bdc7213374e41
andreas.grimme@googlemail.com <andreas.grimme@googlemail.com@8a84ef9e-818e-11dd-b2bb-736fb4d8a922>
2014-12-20 05:35:05 +0000

    Automatic build of sidi-util

---
+++ tags/3.0.0.55/Sidi.Util.Test/IO/PathsTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Sidi.Test;
+
+namespace Sidi.IO
+{
+    [TestFixture]
+    public class PathsTest : TestBase
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        [Test]
+        public void Type()
+        {
+            var p = Paths.Get(GetType());
+            log.Info(p);
+        }
+
+        [Test]
+        public void GetDrives()
+        {
+            foreach (var drive in Paths.GetDrives())
+            {
+                if (new System.IO.DriveInfo(drive).DriveType != System.IO.DriveType.CDRom)
+                {
+                    Assert.IsTrue(drive.IsDirectory, drive.ToString());
+                }
+            }
+        }
+
+        [Test]
+        public void FreeDrives()
+        {
+            foreach (var d in Paths.GetFreeDrives())
+            {
+                Assert.IsFalse(d.Exists);
+                Assert.IsFalse(d.IsDirectory);
+            }
+        }
+    }
+}
