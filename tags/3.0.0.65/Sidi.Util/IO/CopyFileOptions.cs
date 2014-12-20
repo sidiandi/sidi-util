9b2b2ab57d31bf0d84382ec1ae9bfec9485395ad
andreas.grimme@googlemail.com <andreas.grimme@googlemail.com@8a84ef9e-818e-11dd-b2bb-736fb4d8a922>
2014-12-20 10:44:49 +0000

    Automatic build of sidi-util

---
+++ tags/3.0.0.65/Sidi.Util/IO/CopyFileOptions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sidi.IO
+{
+    public class CopyFileOptions
+    {
+        public bool AllowDecryptedDestination { get; set; }
+        public bool CopySymlink { get; set; }
+        public bool FailIfExists { get; set; }
+        public bool NoBuffering { get; set; }
+        public bool OpenSourceForWrite { get; set; }
+        public bool Restartable { get; set; }
+
+        /// <summary>
+        /// Negation of FailIfExists
+        /// </summary>
+        public bool Overwrite
+        {
+            get
+            {
+                return !FailIfExists;
+            }
+
+            set
+            {
+                FailIfExists = !value;
+            }
+        }
+    }
+}
