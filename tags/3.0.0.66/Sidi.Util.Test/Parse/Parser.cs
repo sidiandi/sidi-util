a39612a57f6e319e27f644483914981b23d68981
andreas.grimme@googlemail.com <andreas.grimme@googlemail.com@8a84ef9e-818e-11dd-b2bb-736fb4d8a922>
2014-12-20 11:16:20 +0000

    Automatic build of sidi-util

---
+++ tags/3.0.0.66/Sidi.Util.Test/Parse/Parser.cs
@@ -0,0 +1,81 @@
+﻿using NUnit.Framework;
+using Sidi.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rule = System.Func<Sidi.Parse.Text>;
+
+namespace Sidi.Parse
+{
+    public class Expression : Parser
+    {
+        public static Func<Text, Ast> Expr()
+        {
+            return Enclose(() => Alternative(
+                Concatenation(Term(), Plus(), Term()),
+                Term()
+                ));
+        }
+
+        public static Func<Text, Ast> Symbol(string s)
+        {
+            return Concatenation(OptionalWhitespace(), Expect(s));
+        }
+
+        public static Func<Text, Ast> Plus()
+        {
+            return Collapse(() => Symbol("+"));
+        }
+
+        public static Func<Text, Ast> Term()
+        {
+            return Enclose(() => Alternative(
+                Concatenation(Factor(), Asterisk(), Factor()),
+                Factor()));
+        }
+
+        public static Func<Text, Ast> Factor()
+        {
+            return Enclose(() => Alternative(
+                    Concatenation(Expect("("), Expr(), Expect(")")), 
+                    Number()
+              ));
+        }
+
+        public static Func<Text, Ast> Digits()
+        {
+            return Enclose(() => Repetition(1, Int32.MaxValue, Digit()));
+        }
+
+        public static Func<Text, Ast> Number()
+        {
+            return Collapse(() => Concatenation(OptionalWhitespace(), Digits(), Optional(Concatenation(Expect("."), Digits()))));
+        }
+    }
+
+    [TestFixture]
+    public class ParserTest : TestBase
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        [Test]
+        public void SimpleExpressionParser()
+        {
+            var input = new Text(@" 1");
+            var ast = Expression.Expr()(input);
+            log.Info(ast.Details);
+
+            input = new Text(@"1 + 1");
+            ast = Expression.Expr()(input);
+            log.Info(ast.Details);
+
+            input = new Text(@"1      + 365.1234 * 3");
+            ast = Expression.Expr()(input);
+            log.Info(ast.Details);
+
+            return;
+        }
+    }
+}
