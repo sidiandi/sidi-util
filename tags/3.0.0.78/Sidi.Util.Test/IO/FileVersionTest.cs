a81118e3bb441bb1c4c85bc80987b68282a53dca
andreas.grimme@googlemail.com <andreas.grimme@googlemail.com@8a84ef9e-818e-11dd-b2bb-736fb4d8a922>
2015-01-10 10:14:28 +0000

    Automatic build of sidi-util

---
+++ tags/3.0.0.78/Sidi.Util.Test/IO/FileVersionTest.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using Sidi.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sidi.IO
+{
+    [TestFixture]
+    public class FileVersionTest : TestBase
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        [Test]
+        public void Equal()
+        {
+            var f0 = TestFile("hello");
+            f0.WriteAllText("hello");
+
+            var v0 = new FileVersion(f0);
+            var d0 = new FileVersion(f0.Parent);
+            var v1 = new FileVersion(f0);
+            var d1 = new FileVersion(f0.Parent);
+            Assert.That(v0, Is.EqualTo(v1));
+            Assert.That(d0, Is.EqualTo(d1));
+            log.Info(d1);    
+
+            f0.WriteAllText("hello");
+            v1 = new FileVersion(f0);
+            d1 = new FileVersion(f0.Parent);
+            Assert.That(v0, Is.Not.EqualTo(v1));
+            Assert.That(d0, Is.Not.EqualTo(d1));
+            log.Info(d1);
+        }
+    }
+}
