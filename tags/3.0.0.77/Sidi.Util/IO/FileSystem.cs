5538d719dd8ab89ddfe6407d874f8be3f912b4a9
andreas.grimme@googlemail.com <andreas.grimme@googlemail.com@8a84ef9e-818e-11dd-b2bb-736fb4d8a922>
2015-01-10 10:13:52 +0000

    Automatic build of sidi-util

---
+++ tags/3.0.0.77/Sidi.Util/IO/FileSystem.cs
@@ -0,0 +1,62 @@
+﻿using Sidi.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sidi.IO
+{
+    public class FileSystem
+    {
+        public static IFileSystem Current
+        {
+            get
+            {
+                if (current == null)
+                {
+                    current = DefaultThreadCurrent;
+                }
+                return current;
+            }
+
+            set
+            {
+                current = value;
+            }
+        }
+
+        public static IFileSystem DefaultThreadCurrent
+        {
+            get
+            {
+                lock (syncRoot)
+                {
+                    if (defaultThreadCurrent == null)
+                    {
+                        defaultThreadCurrent = new Windows.FileSystem();
+                    }
+                    return defaultThreadCurrent;
+                }
+            }
+
+            set
+            {
+                defaultThreadCurrent = value;
+            }
+        }
+
+        private static object syncRoot = new Object(); 
+        static IFileSystem defaultThreadCurrent;
+
+        [ThreadStatic]
+        static IFileSystem current = null;
+
+        public static IDisposable SetCurrent(IFileSystem fileSystem)
+        {
+            var originalValue = Current;
+            current = fileSystem;
+            return new OnDispose(() => { current = originalValue; });
+        }
+    }
+}
